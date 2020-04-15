231a6967f7ea938dbe999ad86db08451a6023197
katand <katandps@gmail.com>
2020-04-15 20:52:37 +0900

    2020/04/15演習

---
+++ atcoder/src/try20200415/abc042_a.rs
@@ -0,0 +1,52 @@
+use std::cmp::*;
+use std::io::*;
+use std::num::*;
+use std::str::*;
+
+fn read<T: FromStr>() -> T {
+    let stdin = stdin();
+    let s = stdin
+        .bytes()
+        .map(|c| c.unwrap() as char)
+        .skip_while(|c| c.is_whitespace())
+        .take_while(|c| !c.is_whitespace())
+        .collect::<String>();
+    s.parse::<T>().ok().unwrap()
+}
+
+fn string() -> String {
+    read()
+}
+
+fn int() -> i64 {
+    read()
+}
+
+fn char() -> char {
+    read::<String>().pop().unwrap()
+}
+
+fn vecchar() -> Vec<char> {
+    string().chars().collect()
+}
+
+fn vecint(n: i64) -> Vec<i64> {
+    let mut vec = Vec::new();
+    for i in 0..n {
+        vec.push(int())
+    }
+    vec
+}
+
+fn main() {
+    let (a, b, c) = (int(), int(), int());
+    println!(
+        "{}",
+        match (a, b, c) {
+            (5, 5, 7) => "YES",
+            (5, 7, 5) => "YES",
+            (7, 5, 5) => "YES",
+            _ => "NO",
+        }
+    )
+}
