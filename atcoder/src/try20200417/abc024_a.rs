26b6eaccd9cd3433ab90ec1b85b4f02b3a815758
katand <katandps@gmail.com>
2020-04-17 23:57:12 +0900

    2020/04/17

---
+++ atcoder/src/try20200417/abc024_a.rs
@@ -0,0 +1,52 @@
+use std::cmp::*;
+use std::io::*;
+use std::num::*;
+use std::str::*;
+
+fn read<T: FromStr>() -> T {
+    let stdin = stdin();
+    let s = stdin
+        .bytes()
+        .map(|c| c.unwrap() as char)
+        .skip_while(|c| c.is_whitespace())
+        .take_while(|c| !c.is_whitespace())
+        .collect::<String>();
+    s.parse::<T>().ok().unwrap()
+}
+
+fn string() -> String {
+    read()
+}
+
+fn int() -> i64 {
+    read()
+}
+
+fn char() -> char {
+    read::<String>().pop().unwrap()
+}
+
+fn vecchar() -> Vec<char> {
+    string().chars().collect()
+}
+
+fn vecint(n: i64) -> Vec<i64> {
+    let mut vec = Vec::new();
+    for i in 0..n {
+        vec.push(int())
+    }
+    vec
+}
+
+fn main() {
+    let (a, b, c, k, s, t) = (int(), int(), int(), int(), int(), int());
+    let normal = a * s + t * b;
+    println!(
+        "{}",
+        if s + t >= k {
+            normal - c * (s + t)
+        } else {
+            normal
+        }
+    )
+}
---
+++ atcoder/src/try20200417/abc034_a.rs
@@ -0,0 +1,44 @@
+use std::cmp::*;
+use std::io::*;
+use std::num::*;
+use std::str::*;
+
+fn read<T: FromStr>() -> T {
+    let stdin = stdin();
+    let s = stdin
+        .bytes()
+        .map(|c| c.unwrap() as char)
+        .skip_while(|c| c.is_whitespace())
+        .take_while(|c| !c.is_whitespace())
+        .collect::<String>();
+    s.parse::<T>().ok().unwrap()
+}
+
+fn string() -> String {
+    read()
+}
+
+fn int() -> i64 {
+    read()
+}
+
+fn char() -> char {
+    read::<String>().pop().unwrap()
+}
+
+fn vecchar() -> Vec<char> {
+    string().chars().collect()
+}
+
+fn vecint(n: i64) -> Vec<i64> {
+    let mut vec = Vec::new();
+    for i in 0..n {
+        vec.push(int())
+    }
+    vec
+}
+
+fn main() {
+    let (x, y) = (int(), int());
+    println!("{}", if y > x { "Better" } else { "Worse" })
+}
