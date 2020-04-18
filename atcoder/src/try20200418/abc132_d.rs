1807c143b799f14e9541629d36012c8182a25d64
katand <katandps@gmail.com>
2020-04-19 02:12:16 +0900

    2020/04/18演習

---
+++ atcoder/src/try20200418/abc132_d.rs
@@ -0,0 +1,30 @@
+use input::*;
+use std::cmp::*;
+use std::io::*;
+use std::num::*;
+use std::str::*;
+
+#[rustfmt::skip]
+mod input {
+    use super::*;
+
+    pub fn read<T: FromStr>() -> T { stdin().bytes().map(|c| c.unwrap() as char).skip_while(|c| c.is_whitespace()).take_while(|c| !c.is_whitespace()).collect::<String>().parse::<T>().ok().unwrap() }
+
+    pub fn string() -> String { read() }
+
+    pub fn int() -> i64 { read() }
+
+    pub fn char() -> char { read::<String>().pop().unwrap() }
+
+    pub fn vecchar() -> Vec<char> { string().chars().collect() }
+
+    pub fn vecint(n: i64) -> Vec<i64> {
+        let mut vec = Vec::new();
+        for i in 0..n { vec.push(int()) }
+        vec
+    }
+}
+
+fn main() {
+    let (n, k) = (int(), int());
+}
