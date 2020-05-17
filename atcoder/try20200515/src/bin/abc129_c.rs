856233c9cb65f35680c8d4f1ee00709600fe65ac
katand <katandps@gmail.com>
2020-05-18 01:57:43 +0900

    2020/05/15 演習

---
+++ atcoder/try20200515/src/bin/abc129_c.rs
@@ -0,0 +1,114 @@
+#[allow(dead_code)]
+fn main() {
+    let (n, m) = i::u2();
+
+    let mut a = HashSet::new();
+    for _ in 0..m {
+        a.insert(i::u());
+    }
+
+    let mut dp = vec![1usize];
+    if !a.contains(&1) {
+        dp.push(1);
+    } else {
+        dp.push(0);
+    }
+    for i in 2..(n + 1) {
+        if a.contains(&i) {
+            dp.push(0);
+            continue;
+        }
+        let t = (dp[i - 1] + dp[i - 2]) % 1_000_000_007;
+        dp.push(t);
+    }
+    println!("{}", dp[n]);
+    // dbg!(dp);
+}
+
+#[allow(unused_imports)]
+use std::cmp::*;
+#[allow(unused_imports)]
+use std::collections::{HashMap, HashSet, VecDeque};
+#[allow(unused_imports)]
+use std::io::*;
+#[allow(unused_imports)]
+use std::num::*;
+#[allow(unused_imports)]
+use std::str::*;
+
+#[allow(dead_code)]
+mod i {
+    use super::*;
+
+    pub fn read<T: FromStr>() -> T {
+        stdin()
+            .bytes()
+            .map(|c| c.unwrap() as char)
+            .skip_while(|c| c.is_whitespace())
+            .take_while(|c| !c.is_whitespace())
+            .collect::<String>()
+            .parse::<T>()
+            .ok()
+            .unwrap()
+    }
+
+    pub fn str() -> String {
+        read()
+    }
+
+    pub fn s() -> Vec<char> {
+        str().chars().collect()
+    }
+
+    pub fn i() -> i64 {
+        read()
+    }
+
+    pub fn u() -> usize {
+        read()
+    }
+
+    pub fn u2() -> (usize, usize) {
+        (read(), read())
+    }
+
+    pub fn u3() -> (usize, usize, usize) {
+        (read(), read(), read())
+    }
+
+    pub fn f() -> f64 {
+        read()
+    }
+
+    pub fn c() -> char {
+        read::<String>().pop().unwrap()
+    }
+
+    pub fn iv(n: usize) -> Vec<i64> {
+        (0..n).map(|_| i()).collect()
+    }
+
+    pub fn iv2(n: usize) -> Vec<(i64, i64)> {
+        (0..n).map(|_| iv(2)).map(|a| (a[0], a[1])).collect()
+    }
+
+    pub fn uv(n: usize) -> Vec<usize> {
+        (0..n).map(|_| u()).collect()
+    }
+
+    pub fn uv2(n: usize) -> Vec<(usize, usize)> {
+        (0..n).map(|_| uv(2)).map(|a| (a[0], a[1])).collect()
+    }
+
+    pub fn uv3(n: usize) -> Vec<(usize, usize, usize)> {
+        (0..n).map(|_| uv(3)).map(|a| (a[0], a[1], a[2])).collect()
+    }
+
+    pub fn fv(n: usize) -> Vec<f64> {
+        (0..n).map(|_| f()).collect()
+    }
+
+    pub fn cmap(h: usize) -> Vec<Vec<char>> {
+        (0..h).map(|_| s()).collect()
+    }
+}
