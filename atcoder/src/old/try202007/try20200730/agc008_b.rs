5a2dd54426d342c5addb231652300e8ea5e1045b
katand <katandps@gmail.com>
2020-07-30 22:46:48 +0900

    2020/07/30演習

---
+++ atcoder/src/old/try202007/try20200730/agc008_b.rs
@@ -0,0 +1,168 @@
+#[allow(dead_code)]
+fn main() {
+    let stdin = stdin();
+    let mut reader = StdinReader::new(stdin.lock());
+    let (n, k) = reader.u2();
+    let a = reader.iv(n);
+
+    let mut p = vec![0; n + 1];
+    let mut s = vec![0; n + 1];
+    for i in 0..n {
+        p[i + 1] = p[i] + if a[i] > 0 { a[i] } else { 0 };
+        s[i + 1] = s[i] + a[i];
+    }
+    let mut ans = 0;
+    for i in 0..(n - k + 1) {
+        //k連が黒
+        let a = s[i + k] - s[i] + p[i] + p[n] - p[i + k];
+        ans = max(a, ans);
+        //k連が白
+        let a = p[i] + p[n] - p[i + k];
+        ans = max(a, ans);
+    }
+    println!("{}", ans);
+}
+
+#[allow(unused_imports)]
+use itertools::Itertools;
+#[allow(unused_imports)]
+use std::{cmp::*, collections::*, io::*, num::*, str::*};
+#[allow(unused_imports)]
+use stdin_reader::StdinReader;
+
+#[allow(dead_code)]
+mod stdin_reader {
+    use std::fmt::Debug;
+    use std::io::*;
+    use std::str::*;
+
+    pub struct StdinReader<R: BufRead> {
+        reader: R,
+        buf: Vec<u8>,
+        // Should never be empty
+        pos: usize, // Should never be out of bounds as long as the input ends with '\n'
+    }
+
+    impl<R: BufRead> StdinReader<R> {
+        pub fn new(r: R) -> StdinReader<R> {
+            StdinReader {
+                reader: r,
+                buf: Vec::new(),
+                pos: 0,
+            }
+        }
+        pub fn next<T: FromStr>(&mut self) -> T
+        where
+            T::Err: Debug,
+        {
+            if self.buf.is_empty() {
+                self._read_next_line();
+            }
+            let mut start = None;
+            loop {
+                if self.pos == self.buf.len() {
+                    break;
+                }
+                match (self.buf[self.pos], start.is_some()) {
+                    (b' ', true) | (b'\n', true) => break,
+                    (_, true) | (b' ', false) => self.pos += 1,
+                    (b'\n', false) => self._read_next_line(),
+                    (_, false) => start = Some(self.pos),
+                }
+            }
+            let target = &self.buf[start.unwrap()..self.pos];
+            from_utf8(target).unwrap().parse().unwrap()
+        }
+
+        fn _read_next_line(&mut self) {
+            self.pos = 0;
+            self.buf.clear();
+            if self.reader.read_until(b'\n', &mut self.buf).unwrap() == 0 {
+                panic!("Reached EOF");
+            }
+        }
+
+        pub fn str(&mut self) -> String {
+            self.next()
+        }
+        pub fn s(&mut self) -> Vec<char> {
+            self.next::<String>().chars().collect()
+        }
+        pub fn i(&mut self) -> i64 {
+            self.next()
+        }
+        pub fn i2(&mut self) -> (i64, i64) {
+            (self.next(), self.next())
+        }
+        pub fn i3(&mut self) -> (i64, i64, i64) {
+            (self.next(), self.next(), self.next())
+        }
+        pub fn u(&mut self) -> usize {
+            self.next()
+        }
+        pub fn u2(&mut self) -> (usize, usize) {
+            (self.next(), self.next())
+        }
+        pub fn u3(&mut self) -> (usize, usize, usize) {
+            (self.next(), self.next(), self.next())
+        }
+        pub fn u4(&mut self) -> (usize, usize, usize, usize) {
+            (self.next(), self.next(), self.next(), self.next())
+        }
+        pub fn u5(&mut self) -> (usize, usize, usize, usize, usize) {
+            (
+                self.next(),
+                self.next(),
+                self.next(),
+                self.next(),
+                self.next(),
+            )
+        }
+        pub fn u6(&mut self) -> (usize, usize, usize, usize, usize, usize) {
+            (
+                self.next(),
+                self.next(),
+                self.next(),
+                self.next(),
+                self.next(),
+                self.next(),
+            )
+        }
+        pub fn f(&mut self) -> f64 {
+            self.next()
+        }
+        pub fn f2(&mut self) -> (f64, f64) {
+            (self.next(), self.next())
+        }
+        pub fn c(&mut self) -> char {
+            self.next::<String>().pop().unwrap()
+        }
+        pub fn iv(&mut self, n: usize) -> Vec<i64> {
+            (0..n).map(|_| self.i()).collect()
+        }
+        pub fn iv2(&mut self, n: usize) -> Vec<(i64, i64)> {
+            (0..n).map(|_| self.i2()).collect()
+        }
+        pub fn iv3(&mut self, n: usize) -> Vec<(i64, i64, i64)> {
+            (0..n).map(|_| self.i3()).collect()
+        }
+        pub fn uv(&mut self, n: usize) -> Vec<usize> {
+            (0..n).map(|_| self.u()).collect()
+        }
+        pub fn uv2(&mut self, n: usize) -> Vec<(usize, usize)> {
+            (0..n).map(|_| self.u2()).collect()
+        }
+        pub fn uv3(&mut self, n: usize) -> Vec<(usize, usize, usize)> {
+            (0..n).map(|_| self.u3()).collect()
+        }
+        pub fn uv4(&mut self, n: usize) -> Vec<(usize, usize, usize, usize)> {
+            (0..n).map(|_| self.u4()).collect()
+        }
+        pub fn fv(&mut self, n: usize) -> Vec<f64> {
+            (0..n).map(|_| self.f()).collect()
+        }
+        pub fn cmap(&mut self, h: usize) -> Vec<Vec<char>> {
+            (0..h).map(|_| self.s()).collect()
+        }
+    }
+}
