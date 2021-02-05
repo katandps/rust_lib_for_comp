d481d9ab35b1efb3cd422cfc9d630d61bdde233a
katand <katandps@gmail.com>
2021-02-05 22:04:17 +0900

    fix project runner

--- atcoder/src/lib.rs
+++ atcoder/src/lib.rs
@@ -1,10 +1,51 @@
 mod libraries;
+mod solver;
 mod template;
 
 #[cfg(test)]
 mod tests {
+    use std::io::BufReader;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn a_run() {
+        use crate::solver::a::*;
+        solve(Reader::new(BufReader::new(file("a"))));
+    }
+
+    #[test]
+    fn b_run() {
+        use crate::solver::b::*;
+        solve(Reader::new(BufReader::new(file("b"))));
+    }
+
+    #[test]
+    fn c_run() {
+        use crate::solver::c::*;
+        solve(Reader::new(BufReader::new(file("c"))));
+    }
+
+    #[test]
+    fn d_run() {
+        use crate::solver::d::*;
+        solve(Reader::new(BufReader::new(file("d"))));
+    }
+
+    #[test]
+    fn e_run() {
+        use crate::solver::e::*;
+        solve(Reader::new(BufReader::new(file("e"))));
+    }
+
+    #[test]
+    fn f_run() {
+        use crate::solver::f::*;
+        solve(Reader::new(BufReader::new(file("f"))));
+    }
+
+    fn file(alphabet: &str) -> std::fs::File {
+        let mut path = std::env::current_dir().unwrap();
+        path.push("sample");
+        path.push(format!("{}.txt", alphabet));
+        std::fs::File::open(path).unwrap()
     }
 }
---
+++ atcoder/src/solver.rs
@@ -0,0 +1,6 @@
+pub(crate) mod a;
+pub(crate) mod b;
+pub(crate) mod c;
+pub(crate) mod d;
+pub(crate) mod e;
+pub(crate) mod f;
