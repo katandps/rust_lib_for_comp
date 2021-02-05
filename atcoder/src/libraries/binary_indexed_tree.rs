ef234d72fabbe5eccca364c8e73439a422efd464
katand <katandps@gmail.com>
2021-02-06 00:50:50 +0900

    Change BinaryIndexedTree from 1-indexed to 0-indexed

--- atcoder/src/libraries/binary_indexed_tree.rs
+++ atcoder/src/libraries/binary_indexed_tree.rs
@@ -22,7 +22,9 @@ mod binary_indexed_tree {
             }
         }
 
+        /// add x to i
         pub fn add(&mut self, i: usize, x: VALUE) {
+            let i = i + 1; //0-indexed
             let mut idx = i as i32;
             while idx < self.n as i32 {
                 self.bit[idx as usize] += x;
@@ -30,7 +32,9 @@ mod binary_indexed_tree {
             }
         }
 
+        /// sum of [0, i)
         pub fn sum(&self, i: usize) -> VALUE {
+            let i = i + 1;
             let mut ret = 0;
             let mut idx = i as i32;
             while idx > 0 {
@@ -62,15 +66,32 @@ mod test {
         let mut bit = BinaryIndexedTree::new(LEN);
 
         for _ in 0..1000 {
-            let left = rand::thread_rng().gen_range(1, LEN);
+            let left = rand::thread_rng().gen_range(0, LEN);
             let right = rand::thread_rng().gen_range(left, LEN);
 
-            for i in left..=right {
+            for i in left..right {
                 v[i] += 1;
             }
             bit.add(left, 1);
-            bit.add(right + 1, -1);
+            bit.add(right, -1);
         }
+
+        for i in 0..LEN {
+            assert_eq!(v[i], bit.sum(i));
+        }
+    }
+
+    #[test]
+    fn test_hand() {
+        const LEN: usize = 10;
+        let mut v = vec![0; LEN];
+        let mut bit = BinaryIndexedTree::new(LEN);
+
+        for i in 3..5 {
+            v[i] += 1;
+        }
+        bit.add(3, 1);
+        bit.add(5, -1);
 
         for i in 0..LEN {
             assert_eq!(v[i], bit.sum(i));
