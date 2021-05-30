9dd25ffeb346829956a8c047c652274aa2c682b1
katand <katandps@gmail.com>
2021-05-30 18:32:48 +0900

    ABC192(practice)

---
+++ old/try202105/try20210530/abc192_e.rs
@@ -0,0 +1,98 @@
+#[allow(dead_code)]
+fn main() {
+    let stdin = stdin();
+    solve(Reader::new(stdin.lock()));
+}
+
+pub fn solve<R: BufRead>(mut reader: Reader<R>) {
+    let (n, m, x, y) = reader.u4();
+    let mut a = Vec::new();
+    let mut b = Vec::new();
+    let mut t = Vec::new();
+    let mut k = Vec::new();
+    for _ in 0..m {
+        a.push(reader.u() - 1);
+        b.push(reader.u() - 1);
+        t.push(reader.i());
+        k.push(reader.i());
+    }
+
+    let mut g = vec![Vec::new(); n];
+    for i in 0..m {
+        g[a[i]].push((b[i], t[i], k[i]));
+        g[b[i]].push((a[i], t[i], k[i]));
+    }
+
+    let mut fastest = vec![std::i64::MAX / 10; n];
+    fastest[x - 1] = 0;
+    let mut q = BinaryHeap::new();
+    q.push((Reverse(0), x - 1));
+    while !q.is_empty() {
+        let (Reverse(t), from) = q.pop().unwrap();
+        if fastest[from] < t {
+            continue;
+        }
+        for &(to, t, k) in &g[from] {
+            let time = (fastest[from] + k - 1) / k * k + t;
+            if time < fastest[to] {
+                fastest[to] = time;
+                q.push((Reverse(time), to));
+            }
+        }
+    }
+
+    if fastest[y - 1] == std::i64::MAX / 10 {
+        println!("{}", -1);
+    } else {
+        println!("{}", fastest[y - 1]);
+    }
+}
+
+pub use reader::*;
+#[allow(unused_imports)]
+use {
+    itertools::Itertools,
+    num::Integer,
+    std::{cmp::*, collections::*, io::*, num::*, str::*},
+};
+
+#[allow(dead_code)]
+#[rustfmt::skip]
+pub mod reader {
+    #[allow(unused_imports)]
+    use itertools::Itertools;
+    use std::{fmt::Debug, io::*, str::*};
+
+    pub struct Reader<R: BufRead> {
+        reader: R,
+        buf: Vec<u8>,
+        pos: usize,
+    }  macro_rules! prim_method { ($name:ident: $T: ty) => { pub fn $name(&mut self) -> $T { self.n::<$T>() } }; ($name:ident) => { prim_method!($name: $name); }; } macro_rules! prim_methods { ($name:ident: $T:ty; $($rest:tt)*) => { prim_method!($name:$T); prim_methods!($($rest)*); }; ($name:ident; $($rest:tt)*) => { prim_method!($name); prim_methods!($($rest)*); }; () => () }  macro_rules! replace_expr { ($_t:tt $sub:expr) => { $sub }; } macro_rules! tuple_method { ($name: ident: ($($T:ident),+)) => { pub fn $name(&mut self) -> ($($T),+) { ($(replace_expr!($T self.n())),+) } } } macro_rules! tuple_methods { ($name:ident: ($($T:ident),+); $($rest:tt)*) => { tuple_method!($name:($($T),+)); tuple_methods!($($rest)*); }; () => () } macro_rules! vec_method { ($name: ident: ($($T:ty),+)) => { pub fn $name(&mut self, n: usize) -> Vec<($($T),+)> { (0..n).map(|_|($(replace_expr!($T self.n())),+)).collect_vec() } }; ($name: ident: $T:ty) => { pub fn $name(&mut self, n: usize) -> Vec<$T> { (0..n).map(|_|self.n()).collect_vec() } }; } macro_rules! vec_methods { ($name:ident: ($($T:ty),+); $($rest:tt)*) => { vec_method!($name:($($T),+)); vec_methods!($($rest)*); }; ($name:ident: $T:ty; $($rest:tt)*) => { vec_method!($name:$T); vec_methods!($($rest)*); }; () => () } impl<R: BufRead> Reader<R> {
+    pub fn new(reader: R) -> Reader<R> {
+        let (buf, pos) = (Vec::new(), 0);
+        Reader { reader, buf, pos }
+    } prim_methods! { u: usize; i: i64; f: f64; str: String; c: char; string: String; u8; u16; u32; u64; u128; usize; i8; i16; i32; i64; i128; isize; f32; f64; char; } tuple_methods! { u2: (usize, usize); u3: (usize, usize, usize); u4: (usize, usize, usize, usize); i2: (i64, i64); i3: (i64, i64, i64); i4: (i64, i64, i64, i64); cuu: (char, usize, usize); } vec_methods! { uv: usize; uv2: (usize, usize); uv3: (usize, usize, usize); iv: i64; iv2: (i64, i64); iv3: (i64, i64, i64); vq: (char, usize, usize); }  pub fn n<T: FromStr>(&mut self) -> T where T::Err: Debug, { self.n_op().unwrap() }
+    pub fn n_op<T: FromStr>(&mut self) -> Option<T> where T::Err: Debug, {
+        if self.buf.is_empty() { self._read_next_line(); }
+        let mut start = None;
+        while self.pos != self.buf.len() {
+            match (self.buf[self.pos], start.is_some()) {
+                (b' ', true) | (b'\n', true) => break,
+                (_, true) | (b' ', false) => self.pos += 1,
+                (b'\n', false) => self._read_next_line(),
+                (_, false) => start = Some(self.pos),
+            }
+        }
+        start.map(|s| from_utf8(&self.buf[s..self.pos]).unwrap().parse().unwrap())
+    }
+    fn _read_next_line(&mut self) {
+        self.pos = 0;
+        self.buf.clear();
+        self.reader.read_until(b'\n', &mut self.buf).unwrap();
+    }
+    pub fn s(&mut self) -> Vec<char> { self.n::<String>().chars().collect() }
+    pub fn char_map(&mut self, h: usize) -> Vec<Vec<char>> { (0..h).map(|_| self.s()).collect() }
+    pub fn bool_map(&mut self, h: usize, ng: char) -> Vec<Vec<bool>> { self.char_map(h).iter().map(|v| v.iter().map(|&c| c != ng).collect()).collect() }
+    pub fn matrix(&mut self, h: usize, w: usize) -> Vec<Vec<i64>> { (0..h).map(|_| self.iv(w)).collect() }
+}
+}
