8a63d912454b49446674ea9d62c9f73b114d4df0
katand <katandps@gmail.com>
2021-08-14 13:28:26 +0900

    practice20210812

--- atcoder_lib/src/lib.rs
+++ atcoder_lib/src/lib.rs
@@ -1,3 +1,4 @@
+pub mod algebra;
 pub mod all_combination;
 pub mod all_permutation;
 pub mod binary_indexed_tree;
--- atcoder_lib/src/segment_tree.rs
+++ atcoder_lib/src/segment_tree.rs
@@ -5,62 +5,61 @@ use segment_tree::*;
 pub mod segment_tree {
     use std::fmt::Debug;
 
-    /// 最小値を求めるセグメント木
-    #[derive(Clone, Debug)]
-    pub struct SegmentTree<M> {
+    /// セグメント木
+    /// TはFを二項演算とするモノイド
+    #[derive(Clone)]
+    pub struct SegmentTree<F, T> {
         n: usize,
-        node: Vec<M>,
+        node: Vec<T>,
+        ident: T,
+        eval: F,
     }
 
-    pub trait Monoid: Debug + Clone + Copy {
-        // 0元
-        fn ident() -> Self;
-        fn op(&self, rhs: &Self) -> Self;
-    }
-
-    impl<M: Monoid> SegmentTree<M> {
-        pub fn new(v: &Vec<M>) -> Self {
-            let size = v.len();
-            let mut n = 1;
-            while n < size {
-                n *= 2
-            }
-            let mut node = vec![M::ident(); 2 * n - 1];
-            for i in 0..size {
-                node[i + n - 1] = v[i]
+    impl<F: Fn(T, T) -> T, T: Copy + Eq + Debug> SegmentTree<F, T> {
+        /// 単位元identと二項演算Fの組を使用する、初期値をvとしたセグメント木を生成する
+        pub fn new(v: &Vec<T>, ident: T, eval: F) -> Self {
+            let n = (v.len() + 1).next_power_of_two();
+            let mut node = vec![ident; 2 * n - 1];
+            for i in 0..v.len() {
+                node[i + n] = v[i]
             }
             for i in (0..n - 1).rev() {
-                node[i] = node[2 * i + 1].op(&node[2 * i + 2]);
+                node[i] = eval(node[2 * i + 1], node[2 * i + 2]);
             }
-            Self { n, node }
+            Self {
+                n,
+                node,
+                ident,
+                eval,
+            }
         }
 
-        /// index の値をvalに更新する
-        pub fn set(&mut self, mut index: usize, val: M) {
+        /// index の値をvalで更新する
+        pub fn set(&mut self, mut index: usize, val: T) {
             index += self.n - 1;
-            self.node[index] = val;
+            self.node[index] = (self.eval)(self.node[index], val);
 
             while index > 0 {
                 index = (index - 1) / 2;
-                self.node[index] = self.node[2 * index + 1].op(&self.node[2 * index + 2]);
+                self.node[index] = (self.eval)(self.node[2 * index + 1], self.node[2 * index + 2]);
             }
         }
 
         /// get for [a, b)
-        pub fn get(&self, a: usize, b: usize) -> M {
+        pub fn get(&self, a: usize, b: usize) -> T {
             self.g(a, b, None, None, None)
         }
 
-        fn g(&self, a: usize, b: usize, k: Option<usize>, l: Option<usize>, r: Option<usize>) -> M {
+        fn g(&self, a: usize, b: usize, k: Option<usize>, l: Option<usize>, r: Option<usize>) -> T {
             let (k, l, r) = (k.unwrap_or(0), l.unwrap_or(0), r.unwrap_or(self.n));
             if r <= a || b <= l {
-                M::ident()
+                self.ident
             } else if a <= l && r <= b {
                 self.node[k]
             } else {
                 let vl = self.g(a, b, Some(2 * k + 1), Some(l), Some((l + r) / 2));
                 let vr = self.g(a, b, Some(2 * k + 2), Some((l + r) / 2), Some(r));
-                vl.op(&vr)
+                (self.eval)(vl, vr)
             }
         }
     }
