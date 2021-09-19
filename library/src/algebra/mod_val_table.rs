52f151d58452280812ca0935a7822d7ca76c4c8a
katand <katandps@gmail.com>
2021-09-20 02:02:53 +0900

    mod_val_tableの依存が壊れていたので修正

--- library/src/algebra/mod_val_table.rs
+++ library/src/algebra/mod_val_table.rs
@@ -3,7 +3,7 @@ pub use mod_val_table::ModValTable;
 
 #[allow(dead_code)]
 mod mod_val_table {
-    use crate::algebra::mod_int::mod_int::*;
+    use super::*;
 
     /// 剰余類Modについて、組み合わせや順列を数え上げる
     #[derive(std::fmt::Debug)]
@@ -111,4 +111,4 @@ mod mod_val_table {
 
 ////////////////////////////////////////////////////////
 
-use crate::algebra::mod_int::mod_int;
+use crate::algebra::mod_int::mod_int::*;
