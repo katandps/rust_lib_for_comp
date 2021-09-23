d2e9cf39ebb9ea4fab2816931d4c52aa196b6b39
katand <katandps@gmail.com>
2021-09-23 15:08:41 +0900

    2次元BITにデバッグTraitを実装

--- library/src/tree/binary_indexed_tree_2d.rs
+++ library/src/tree/binary_indexed_tree_2d.rs
@@ -49,4 +49,21 @@ pub mod binary_indexed_tree_2d {
             self.sum(y2, x2) - self.sum(y2, x1) - self.sum(y1, x2) + self.sum(y1, x1)
         }
     }
+
+    impl std::fmt::Debug for BinaryIndexedTree2 {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            let mut buf = String::new();
+            buf += "\n";
+            for y in 0..self.h - 1 {
+                for x in 0..self.w - 1 {
+                    if x > 0 {
+                        buf += " ";
+                    }
+                    buf += self.sum(y, x).to_string().as_str();
+                }
+                buf += "\n";
+            }
+            write!(f, "{}", buf)
+        }
+    }
 }
