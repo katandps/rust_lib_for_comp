af60dfa60082dec4887cc5e33ebf5a025a8ecac7
katand <katandps@gmail.com>
2021-10-25 20:57:21 +0900

    readerの改行コード対応

--- library/src/algo/reader.rs
+++ library/src/algo/reader.rs
@@ -3,136 +3,53 @@ use crate::*;
 
 pub struct Reader<R: BufRead> {
     reader: R,
-    buf: Vec<u8>,
-    pos: usize,
+    buf: VecDeque<String>,
 }
 
-macro_rules! prim_method {
-    ($name:ident: $T: ty) => {
-        pub fn $name(&mut self) -> $T {
-            self.n::<$T>()
-        }
-    };
-    ($name:ident) => {
-        prim_method!($name: $name);
-    }
-}
-macro_rules! prim_methods {
-    ($name:ident: $T:ty; $($rest:tt)*) => {
-        prim_method!($name:$T);
-        prim_methods!($($rest)*);
-    };
-    ($name:ident; $($rest:tt)*) => {
-        prim_method!($name);
-        prim_methods!($($rest)*);
-    };
-    () => ()
-}
-
-macro_rules! replace_expr {
-    ($_t:tt $sub:expr) => {
-        $sub
-    };
-}
-macro_rules! tuple_method {
-    ($name: ident: ($($T:ident),+)) => {
-        pub fn $name(&mut self) -> ($($T),+) {
-            ($(replace_expr!($T self.n())),+)
-        }
-    }
-}
-macro_rules! tuple_methods {
-    ($name:ident: ($($T:ident),+); $($rest:tt)*) => {
-        tuple_method!($name:($($T),+));
-        tuple_methods!($($rest)*);
-    };
-    () => ()
-}
-macro_rules! vec_method {
-    ($name: ident: ($($T:ty),+)) => {
-        pub fn $name(&mut self, n: usize) -> Vec<($($T),+)> {
-            (0..n).map(|_|($(replace_expr!($T self.n())),+)).collect()
-        }
-    };
-    ($name: ident: $T:ty) => {
-        pub fn $name(&mut self, n: usize) -> Vec<$T> {
-            (0..n).map(|_|self.n()).collect()
-        }
-    };
-}
-macro_rules! vec_methods {
-    ($name:ident: ($($T:ty),+); $($rest:tt)*) => {
-        vec_method!($name:($($T),+));
-        vec_methods!($($rest)*);
-    };
-    ($name:ident: $T:ty; $($rest:tt)*) => {
-        vec_method!($name:$T);
-        vec_methods!($($rest)*);
-    };
-    () => ()
-}
 impl<R: BufRead> Reader<R> {
     pub fn new(reader: R) -> Reader<R> {
-        let (buf, pos) = (Vec::new(), 0);
-        Reader { reader, buf, pos }
-    }
-    prim_methods! {
-        u: usize; i: i64; f: f64; str: String; c: char; string: String;
-        u8; u16; u32; u64; u128; usize; i8; i16; i32; i64; i128; isize; f32; f64; char;
-    }
-    tuple_methods! {
-        u2: (usize, usize); u3: (usize, usize, usize); u4: (usize, usize, usize, usize);
-        i2: (i64, i64); i3: (i64, i64, i64); i4: (i64, i64, i64, i64);
-        cuu: (char, usize, usize);
-    }
-    vec_methods! {
-        uv: usize; uv2: (usize, usize); uv3: (usize, usize, usize);
-        iv: i64; iv2: (i64, i64); iv3: (i64, i64, i64);
-        vq: (char, usize, usize);
+        Reader {
+            reader,
+            buf: VecDeque::new(),
+        }
     }
 
-    pub fn n<T: FromStr>(&mut self) -> T
+    pub fn next<T>(&mut self) -> T
     where
-        T::Err: Debug,
-    {
-        self.n_op().unwrap()
-    }
-
-    pub fn n_op<T: FromStr>(&mut self) -> Option<T>
-    where
+        T: FromStr,
         T::Err: Debug,
     {
         if self.buf.is_empty() {
-            self._read_next_line();
+            let mut buf = Vec::new();
+            self.reader.read_to_end(&mut buf).unwrap();
+            let s = from_utf8(&buf).expect("utf8でない文字列が入力されました.");
+            s.split_whitespace()
+                .map(ToString::to_string)
+                .for_each(|s| self.buf.push_back(s));
         }
-        let mut start = None;
-        while self.pos != self.buf.len() {
-            match (self.buf[self.pos], start.is_some()) {
-                (b' ', true) | (b'\n', true) => break,
-                (_, true) | (b' ', false) => self.pos += 1,
-                (b'\n', false) => self._read_next_line(),
-                (_, false) => start = Some(self.pos),
-            }
+        match self.buf.pop_front() {
+            Some(token) => token.parse().unwrap(),
+            _ => panic!("入力が足りません。"),
         }
-        start.map(|s| from_utf8(&self.buf[s..self.pos]).unwrap().parse().unwrap())
     }
-
-    fn _read_next_line(&mut self) {
-        self.pos = 0;
-        self.buf.clear();
-        self.reader.read_until(b'\n', &mut self.buf).unwrap();
+    pub fn vec<T>(&mut self, length: usize) -> Vec<T>
+    where
+        T: FromStr,
+        T::Err: Debug,
+    {
+        (0..length).map(|_| self.next()).collect()
     }
-    pub fn s(&mut self) -> Vec<char> {
-        self.n::<String>().chars().collect()
+    pub fn chars(&mut self) -> Vec<char> {
+        self.next::<String>().chars().collect()
     }
     pub fn digits(&mut self) -> Vec<i64> {
-        self.n::<String>()
+        self.next::<String>()
             .chars()
             .map(|c| (c as u8 - b'0') as i64)
             .collect()
     }
     pub fn char_map(&mut self, h: usize) -> Vec<Vec<char>> {
-        (0..h).map(|_| self.s()).collect()
+        (0..h).map(|_| self.chars()).collect()
     }
     /// charの2次元配列からboolのmapを作る ngで指定した壁のみfalseとなる
     pub fn bool_map(&mut self, h: usize, ng: char) -> Vec<Vec<bool>> {
@@ -142,8 +59,12 @@ impl<R: BufRead> Reader<R> {
             .collect()
     }
     /// h*w行列を取得する
-    pub fn matrix(&mut self, h: usize, w: usize) -> Vec<Vec<i64>> {
-        (0..h).map(|_| self.iv(w)).collect()
+    pub fn matrix<T>(&mut self, h: usize, w: usize) -> Vec<Vec<T>>
+    where
+        T: FromStr,
+        T::Err: Debug,
+    {
+        (0..h).map(|_| self.vec(w)).collect()
     }
 }
 
@@ -155,19 +76,29 @@ mod tests {
 
     #[test]
     fn basics() {
-        let cursor = Cursor::new(b"-123 456.7 12345 Hello, world!\n");
-        let mut reader = Reader::new(cursor);
+        {
+            let cursor = Cursor::new(b"-123 456.7 12345\nhogehoge\n");
+            let mut reader = Reader::new(cursor);
 
-        assert_eq!(-123, reader.i());
-        assert_eq!(456.7, reader.f());
-        assert_eq!(12345, reader.u());
-        assert_eq!("Hello,".to_string(), reader.str());
-        assert_eq!("world!".to_string(), reader.str());
+            assert_eq!(-123, reader.next());
+            assert_eq!(456.7, reader.next());
+            assert_eq!(12345, reader.next());
+            assert_eq!("hogehoge".to_string(), reader.next::<String>());
+        }
+        {
+            let cursor = Cursor::new(b"-123 456.7 12345\rhogehoge\r");
+            let mut reader = Reader::new(cursor);
 
-        let cursor = Cursor::new(b"123 456 789 012 345 678\n");
-        let mut reader = Reader::new(cursor);
-
-        assert_eq!(vec![123, 456, 789, 12, 345, 678], reader.uv(6));
+            assert_eq!(-123, reader.next());
+            assert_eq!(456.7, reader.next());
+            assert_eq!(12345, reader.next());
+            assert_eq!("hogehoge".to_string(), reader.next::<String>());
+        }
+        {
+            let cursor = Cursor::new(b"123 456 789 012 345 678\n");
+            let mut reader = Reader::new(cursor);
+            assert_eq!(vec![123, 456, 789, 12, 345, 678], reader.vec(6));
+        }
     }
 
     #[test]
@@ -175,18 +106,18 @@ mod tests {
         {
             let cursor = Cursor::new(b"8\n");
             let mut reader = Reader::new(cursor);
-            assert_eq!(8u32, reader.n());
+            assert_eq!(8u32, reader.next());
         }
         {
             let cursor = Cursor::new(b"\n9\n");
             let mut reader = Reader::new(cursor);
-            assert_eq!(9i32, reader.n());
+            assert_eq!(9i32, reader.next());
         }
         {
             let cursor = Cursor::new(b"\n\n10\n11\n");
             let mut reader = Reader::new(cursor);
-            assert_eq!(10u8, reader.n());
-            assert_eq!(11u8, reader.n());
+            assert_eq!(10u8, reader.next());
+            assert_eq!(11u8, reader.next());
         }
     }
 
--- library/src/template.rs
+++ library/src/template.rs
@@ -38,146 +38,52 @@ macro_rules! max {
 /// stdin reader
 pub struct Reader<R: BufRead> {
     reader: R,
-    buf: Vec<u8>,
-    pos: usize,
-}
-
-macro_rules! prim_method {
-    ($name:ident: $T: ty) => {
-        pub fn $name(&mut self) -> $T {
-            self.n::<$T>()
-        }
-    };
-    ($name:ident) => {
-        prim_method!($name: $name);
-    }
-}
-macro_rules! prim_methods {
-    ($name:ident: $T:ty; $($rest:tt)*) => {
-        prim_method!($name:$T);
-        prim_methods!($($rest)*);
-    };
-    ($name:ident; $($rest:tt)*) => {
-        prim_method!($name);
-        prim_methods!($($rest)*);
-    };
-    () => ()
-}
-
-macro_rules! replace_expr {
-    ($_t:tt $sub:expr) => {
-        $sub
-    };
-}
-macro_rules! tuple_method {
-    ($name: ident: ($($T:ident),+)) => {
-        pub fn $name(&mut self) -> ($($T),+) {
-            ($(replace_expr!($T self.n())),+)
-        }
-    }
-}
-macro_rules! tuple_methods {
-    ($name:ident: ($($T:ident),+); $($rest:tt)*) => {
-        tuple_method!($name:($($T),+));
-        tuple_methods!($($rest)*);
-    };
-    () => ()
-}
-macro_rules! vec_method {
-    ($name: ident: ($($T:ty),+)) => {
-        pub fn $name(&mut self, n: usize) -> Vec<($($T),+)> {
-            (0..n).map(|_|($(replace_expr!($T self.n())),+)).collect()
-        }
-    };
-    ($name: ident: $T:ty) => {
-        pub fn $name(&mut self, n: usize) -> Vec<$T> {
-            (0..n).map(|_|self.n()).collect()
-        }
-    };
-}
-macro_rules! vec_methods {
-    ($name:ident: ($($T:ty),+); $($rest:tt)*) => {
-        vec_method!($name:($($T),+));
-        vec_methods!($($rest)*);
-    };
-    ($name:ident: $T:ty; $($rest:tt)*) => {
-        vec_method!($name:$T);
-        vec_methods!($($rest)*);
-    };
-    () => ()
+    buf: VecDeque<String>,
 }
 impl<R: BufRead> Reader<R> {
     pub fn new(reader: R) -> Reader<R> {
-        let (buf, pos) = (Vec::new(), 0);
-        Reader { reader, buf, pos }
+        Reader {
+            reader,
+            buf: VecDeque::new(),
+        }
     }
-    prim_methods! {
-        u: usize; i: i64; f: f64; str: String; c: char; string: String; u8; u16; u32; u64; u128; usize; i8; i16; i32; i64; i128; isize; f32; f64; char;
-    }
-    tuple_methods! {
-        u2: (usize, usize); u3: (usize, usize, usize); u4: (usize, usize, usize, usize);
-        i2: (i64, i64); i3: (i64, i64, i64); i4: (i64, i64, i64, i64);
-        cuu: (char, usize, usize);
-    }
-    vec_methods! {
-        uv: usize; uv2: (usize, usize); uv3: (usize, usize, usize);
-        iv: i64; iv2: (i64, i64); iv3: (i64, i64, i64);
-        vq: (char, usize, usize);
-    }
-
-    pub fn n<T: FromStr>(&mut self) -> T
-    where
-        T::Err: Debug,
-    {
-        self.n_op().unwrap()
-    }
-
-    pub fn n_op<T: FromStr>(&mut self) -> Option<T>
-    where
-        T::Err: Debug,
-    {
+    pub fn next<T: FromStr>(&mut self) -> T {
         if self.buf.is_empty() {
-            self._read_next_line();
+            let mut buf = Vec::new();
+            self.reader.read_to_end(&mut buf).unwrap();
+            let s = from_utf8(&buf).expect("utf8でない文字列が入力されました.");
+            s.split_whitespace()
+                .map(ToString::to_string)
+                .for_each(|s| self.buf.push_back(s));
         }
-        let mut start = None;
-        while self.pos != self.buf.len() {
-            match (self.buf[self.pos], start.is_some()) {
-                (b' ', true) | (b'\n', true) => break,
-                (_, true) | (b' ', false) => self.pos += 1,
-                (b'\n', false) => self._read_next_line(),
-                (_, false) => start = Some(self.pos),
-            }
+        match self.buf.pop_front() {
+            Some(token) => token.parse().ok().expect("型変換エラーです."),
+            _ => panic!("入力が足りません."),
         }
-        start.map(|s| from_utf8(&self.buf[s..self.pos]).unwrap().parse().unwrap())
     }
-
-    fn _read_next_line(&mut self) {
-        self.pos = 0;
-        self.buf.clear();
-        self.reader.read_until(b'\n', &mut self.buf).unwrap();
+    pub fn vec<T: FromStr>(&mut self, length: usize) -> Vec<T> {
+        (0..length).map(|_| self.next()).collect()
     }
-    pub fn s(&mut self) -> Vec<char> {
-        self.n::<String>().chars().collect()
+    pub fn chars(&mut self) -> Vec<char> {
+        self.next::<String>().chars().collect()
     }
     pub fn digits(&mut self) -> Vec<i64> {
-        self.n::<String>()
+        self.next::<String>()
             .chars()
             .map(|c| (c as u8 - b'0') as i64)
             .collect()
     }
     pub fn char_map(&mut self, h: usize) -> Vec<Vec<char>> {
-        (0..h).map(|_| self.s()).collect()
+        (0..h).map(|_| self.chars()).collect()
     }
-    /// charの2次元配列からboolのmapを作る ngで指定した壁のみfalseとなる
     pub fn bool_map(&mut self, h: usize, ng: char) -> Vec<Vec<bool>> {
         self.char_map(h)
             .iter()
             .map(|v| v.iter().map(|&c| c != ng).collect())
             .collect()
     }
-    /// h*w行列を取得する
-    pub fn matrix(&mut self, h: usize, w: usize) -> Vec<Vec<i64>> {
-        (0..h).map(|_| self.iv(w)).collect()
+    pub fn matrix<T: FromStr>(&mut self, h: usize, w: usize) -> Vec<Vec<T>> {
+        (0..h).map(|_| self.vec(w)).collect()
     }
 }
 
@@ -223,6 +129,6 @@ fn main() {
 
 pub fn solve<R: BufRead, W: Write>(mut reader: Reader<R>, mut writer: Writer<W>) {
     //$END$//
-    let n = reader.u();
+    let n = reader.next::<usize>();
     writer.println(&n);
 }
