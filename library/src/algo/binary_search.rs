01143da815ba219f581dd31c9aa3aa9211ea3ed8
katand <katandps@gmail.com>
2021-10-26 18:25:22 +0900

    二分探索のライブラリを修正

--- library/src/algo/binary_search.rs
+++ library/src/algo/binary_search.rs
@@ -1,12 +1,11 @@
-//! 二分探索(コピペ用)
-
-#[allow(dead_code)]
-fn search() -> isize {
-    let mut ng: isize = -1;
-    let mut ok: isize = 100;
+//! # 二分探索
+//!
+//! 整数での二分探索を行う
+//!
+pub fn binary_search<F: Fn(i64) -> bool>(mut ok: i64, mut ng: i64, f: F) -> i64 {
     while (ok - ng).abs() > 1 {
         let mid = (ok + ng) / 2;
-        if is_ok(mid) {
+        if f(mid) {
             ok = mid
         } else {
             ng = mid
@@ -15,6 +14,13 @@ fn search() -> isize {
     ok
 }
 
-fn is_ok(_key: isize) -> bool {
-    true
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn it_works() {
+        let src = vec![1, 5, 15, 30, 55, 90, 150];
+        let res = binary_search(src.len() as i64, -1, |i| src[i as usize] > 60);
+        assert_eq!(src[res as usize], 90);
+    }
 }
