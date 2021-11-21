d6a97a17a5c6f0c6e63a29d4ecc4fee36927c783
katand <katandps@gmail.com>
2021-11-22 03:43:10 +0900

    mod_powのバグ修正

--- src/algebra/mod_pow.rs
+++ src/algebra/mod_pow.rs
@@ -7,16 +7,20 @@
 use crate::prelude::*;
 
 #[snippet(name = "mod-pow", doc_hidden)]
-pub fn pow(mut p: usize, mut e: usize, m: usize) -> usize {
+/// # $`p^e \bmod m`$
+/// ```
+/// use rust_lib_for_comp::algebra::mod_pow::pow;
+/// assert_eq!(130944741, pow(12738078907407, 9982443567 , 998244353))
+/// ```
+pub fn pow(mut p: i64, mut e: i64, m: i64) -> i64 {
+    p %= m;
     let mut res = 1;
     while e > 0 {
         if e & 1 == 1 {
-            res *= p;
-            res %= m;
+            res = res * p % m;
         }
         e >>= 1;
-        p *= p;
-        p %= m;
+        p = p * p % m;
     }
     res
 }
