3a66a5332c4c9650a5e5e41560043b070ac4ebfe
katand <katandps@gmail.com>
2021-12-02 10:58:37 +0900

    二分探索のテストをドキュメンテーションテストに変更

--- src/algo/binary_search.rs
+++ src/algo/binary_search.rs
@@ -1,10 +1,19 @@
 //! # 二分探索
 //!
 //! 整数での二分探索を行う
+//!
+//! ## 使い方
+//!
+//! ```
+//! # use rust_lib_for_comp::algo::binary_search::*;
+//! let src = vec![1, 5, 15, 30, 55, 90, 150];
+//! let res = binary_search(src.len() as i64, -1, |i| src[i as usize] > 55);
+//! assert_eq!(src[res as usize], 90);
+//! ```
 
 use crate::prelude::*;
 
-#[snippet(name = "binary_search", doc_hidden)]
+#[snippet(name = "binary-search", doc_hidden)]
 pub fn binary_search<F: Fn(i64) -> bool>(mut ok: i64, mut ng: i64, f: F) -> i64 {
     while (ok - ng).abs() > 1 {
         let mid = (ok + ng) / 2;
@@ -15,15 +24,4 @@ pub fn binary_search<F: Fn(i64) -> bool>(mut ok: i64, mut ng: i64, f: F) -> i64 
         }
     }
     ok
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-    #[test]
-    fn it_works() {
-        let src = vec![1, 5, 15, 30, 55, 90, 150];
-        let res = binary_search(src.len() as i64, -1, |i| src[i as usize] > 60);
-        assert_eq!(src[res as usize], 90);
-    }
 }
