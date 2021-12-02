35e4cfa60dedc9b467c1cb782f1dc683d395ffc8
katand <katandps@gmail.com>
2021-12-02 10:57:57 +0900

    二部グラフ判定ライブラリが使えなかったのを修正

--- src/graph/bipartite_graph.rs
+++ src/graph/bipartite_graph.rs
@@ -1,29 +1,40 @@
-//! # 二部グラフ判定
+//! # 二部グラフ
 //! グラフが二部グラフかどうか判定する
 //! 二部グラフだったときはその分割方法を1つ返す
 //! なお、グラフが連結でない場合は正しく判定できない
 use crate::graph::GraphTrait;
 use crate::prelude::*;
 
-#[snippet(name = "judge-bipartite-graph", doc_hidden)]
-impl<W> dyn GraphTrait<Weight = W> {
-    pub fn is_bipartite_graph(&self) -> Option<Vec<bool>> {
-        let mut dist = vec![None; self.size()];
+#[snippet(name = "bipartite-graph", doc_hidden)]
+pub struct BipartiteGraph(Vec<bool>);
+
+#[snippet(name = "bipartite-graph", doc_hidden)]
+impl<G: GraphTrait> From<G> for BipartiteGraph {
+    fn from(graph: G) -> Self {
+        let mut dist = vec![None; graph.size()];
         dist[0] = Some(true);
-        if self.dfs(0, &mut dist) {
-            Some(dist.iter().map(|op| op.unwrap()).collect())
+        if Self::dfs(&graph, 0, &mut dist) {
+            Self(dist.iter().map(|op| op.unwrap()).collect())
         } else {
-            None
+            Self(Vec::new())
         }
     }
-    fn dfs(&self, src: usize, d: &mut Vec<Option<bool>>) -> bool {
-        for edge in &self.edges(src) {
+}
+
+#[snippet(name = "bipartite-graph", doc_hidden)]
+impl BipartiteGraph {
+    pub fn is_bipartite(&self) -> bool {
+        !self.0.is_empty()
+    }
+
+    fn dfs<G: GraphTrait>(graph: &G, src: usize, d: &mut Vec<Option<bool>>) -> bool {
+        for edge in graph.edges(src) {
             if d[edge.dst] == d[src] {
                 return false;
             }
             if d[edge.dst].is_none() {
                 d[edge.dst] = d[src].map(|b| !b);
-                if !self.dfs(edge.dst, d) {
+                if !Self::dfs(graph, edge.dst, d) {
                     return false;
                 }
             }
