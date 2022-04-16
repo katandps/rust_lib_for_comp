361cf1012f22044748c0cab5b4f77d1d66dbd077
katand <katandps@gmail.com>
2022-04-16 20:46:45 +0900

    primal_dualのリファクタリング

--- src/flow/primal_dual.rs
+++ src/flow/primal_dual.rs
@@ -2,7 +2,7 @@
 //! 最小費用流問題を最短路反復(with Dijkstra)で解く。
 //!
 //! ## verify
-//! [GRL_6_B](https://judge.u-aizu.ac.jp/onlinejudge/review.jsp?rid=6482630#2)
+//! [GRL_6_B](https://judge.u-aizu.ac.jp/onlinejudge/review.jsp?rid=6493214)
 use crate::graph::adjacency_list::Graph;
 use crate::graph::GraphTrait;
 use crate::prelude::*;
@@ -43,60 +43,48 @@ where
         self.rev[j] = i;
     }
 
-    pub fn min_cost_flow(&mut self, s: usize, t: usize, mut f: Fl) -> Option<Fl> {
-        let v = self.graph.size();
-        let mut ret = Fl::zero();
+    // returns (各頂点までの最短距離, 頂点に向かう辺)
+    fn dijkstra(&self, s: usize, potential: &mut Vec<Co>) -> (Vec<Option<Co>>, Vec<Option<usize>>) {
+        let mut min_cost = vec![None; self.graph.size()];
+        let mut prev: Vec<Option<usize>> = vec![None; self.graph.size()];
         let mut pq: BinaryHeap<Reverse<(Co, usize)>> = BinaryHeap::new();
-        let mut potential = vec![Co::zero(); v];
-        while f > Fl::zero() {
-            let mut min_cost = vec![None; v];
-            let mut prev_edge: Vec<Option<usize>> = vec![None; v];
-            min_cost[s] = Some(Co::zero());
-            pq.push(Reverse((Co::zero(), s)));
-            while let Some(Reverse((cost, src))) = pq.pop() {
-                if min_cost[src] < Some(cost) {
+        min_cost[s] = Some(Co::zero());
+        pq.push(Reverse((Co::zero(), s)));
+        while let Some(Reverse((cost, src))) = pq.pop() {
+            if min_cost[src] < Some(cost) {
+                continue;
+            }
+            for &i in &self.graph.index[src] {
+                let (_src, dst, (cap, cost)) = self.graph.edges[i];
+                if cap == Fl::zero() {
                     continue;
                 }
-                for &i in &self.graph.index[src] {
-                    let (_src, dst, (cap, cost)) = self.graph.edges[i];
-                    if cap == Fl::zero() {
-                        continue;
-                    }
-                    if let Some(m) = min_cost[src] {
-                        let next_cost = m + cost + potential[src] - potential[dst];
-                        if min_cost[dst].is_none() || min_cost[dst] > Some(next_cost) {
-                            min_cost[dst] = Some(next_cost);
-                            prev_edge[dst] = Some(i);
-                            pq.push(Reverse((min_cost[dst].unwrap(), dst)))
-                        }
+                if let Some(m) = min_cost[src] {
+                    let next_cost = m + cost + potential[src] - potential[dst];
+                    if min_cost[dst].is_none() || min_cost[dst] > Some(next_cost) {
+                        min_cost[dst] = Some(next_cost);
+                        prev[dst] = Some(i);
+                        pq.push(Reverse((min_cost[dst].unwrap(), dst)))
                     }
                 }
-            }
-            min_cost[t]?;
-            (0..v).for_each(|i| {
-                if let Some(m) = min_cost[i] {
-                    potential[i] += m
-                }
-            });
-            let mut addflow = f;
-            let mut cur = t;
-            while cur != s {
-                let prev_i = prev_edge[cur].unwrap();
-                let (src, _dst, (cap, _cost)) = self.graph.edges[prev_i];
-                chmin!(addflow, cap);
-                cur = src;
-            }
-            f -= addflow;
-            ret += addflow * potential[t];
-            let mut cur = t;
-            while cur != s {
-                let prev_i = prev_edge[cur].unwrap();
-                self.graph.edges[prev_i].2 .0 -= addflow;
-                self.graph.edges[self.rev[prev_i]].2 .0 += addflow;
-                cur = self.graph.edges[prev_i].0;
             }
         }
-        Some(ret)
+        (min_cost, prev)
+    }
+
+    pub fn min_cost_flow(&mut self, s: usize, t: usize, f: Fl) -> Option<Fl> {
+        let slope = self.slope(s, t, f);
+        let mut total_flow = Fl::zero();
+        let mut ret = Fl::zero();
+        for (flow, potential) in slope {
+            total_flow += flow;
+            ret += flow * potential
+        }
+        if total_flow == f {
+            Some(ret)
+        } else {
+            None
+        }
     }
 
     /// # 流量最大fまでの傾斜を求める
@@ -104,52 +92,29 @@ where
     /// ## verify
     /// [ABC247G](https://atcoder.jp/contests/abc247/submissions/30989081)
     pub fn slope(&mut self, s: usize, t: usize, mut f: Fl) -> Vec<(Fl, Co)> {
-        let v = self.graph.size();
         let mut ret = vec![(Fl::zero(), Co::zero())];
-        let mut pq: BinaryHeap<Reverse<(Co, usize)>> = BinaryHeap::new();
-        let mut potential = vec![Co::zero(); v];
+        let mut potential = vec![Co::zero(); self.graph.size()];
         while f > Fl::zero() {
-            let mut min_cost = vec![None; v];
-            let mut prev_edge: Vec<Option<usize>> = vec![None; v];
-            min_cost[s] = Some(Co::zero());
-            pq.push(Reverse((Co::zero(), s)));
-            while let Some(Reverse((cost, src))) = pq.pop() {
-                if min_cost[src] < Some(cost) {
-                    continue;
-                }
-                for &i in &self.graph.index[src] {
-                    let (_src, dst, (cap, cost)) = self.graph.edges[i];
-                    if cap == Fl::zero() {
-                        continue;
-                    }
-                    if let Some(m) = min_cost[src] {
-                        let next_cost = m + cost + potential[src] - potential[dst];
-                        if min_cost[dst].is_none() || min_cost[dst] > Some(next_cost) {
-                            min_cost[dst] = Some(next_cost);
-                            prev_edge[dst] = Some(i);
-                            pq.push(Reverse((min_cost[dst].unwrap(), dst)))
-                        }
-                    }
-                }
-            }
+            let (min_cost, prev_edge) = self.dijkstra(s, &mut potential);
             if min_cost[t].is_none() {
                 break;
             }
-            (0..v).for_each(|i| {
+            (0..self.graph.size()).for_each(|i| {
                 if let Some(m) = min_cost[i] {
                     potential[i] += m
                 }
             });
-            let mut addflow = f;
-            let mut cur = t;
-            while cur != s {
-                let prev_i = prev_edge[cur].unwrap();
-                let (src, _dst, (cap, _cost)) = self.graph.edges[prev_i];
-                chmin!(addflow, cap);
-                cur = src;
-            }
-            f -= addflow;
-            ret.push((addflow, potential[t]));
+            let addflow = {
+                let mut addflow = f;
+                let mut cur = t;
+                while cur != s {
+                    let prev_i = prev_edge[cur].unwrap();
+                    chmin!(addflow, self.graph.edges[prev_i].2 .0);
+                    cur = self.graph.edges[prev_i].0;
+                }
+                addflow
+            };
+            //　tからsへの経路について、流したflowをグラフから取り除く
             let mut cur = t;
             while cur != s {
                 let prev_i = prev_edge[cur].unwrap();
@@ -157,6 +122,8 @@ where
                 self.graph.edges[self.rev[prev_i]].2 .0 += addflow;
                 cur = self.graph.edges[prev_i].0;
             }
+            f -= addflow;
+            ret.push((addflow, potential[t]));
         }
         ret
     }
