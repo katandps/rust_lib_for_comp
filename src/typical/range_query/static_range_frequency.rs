c46515040fc4e9bf2a76f764abc79530eec734cd
katand <katandps@gmail.com>
2022-04-20 18:33:24 +0900

    典型問題の解答を入出力に依存しないように修正

--- src/typical/range_query/static_range_frequency.rs
+++ src/typical/range_query/static_range_frequency.rs
@@ -13,30 +13,33 @@
 //! ```
 //!
 
-use crate::{algo::slice_bounds::SliceBounds, prelude::*};
+use crate::algo::slice_bounds::SliceBounds;
 
 #[allow(clippy::many_single_char_names)]
-pub fn solve<R: BufRead, W: Write, F: FnMut() -> R>(mut reader: Reader<F>, mut writer: Writer<W>) {
-    let (n, q) = reader.v2::<usize, usize>();
-    let a = reader.vec::<usize>(n);
+pub fn solve(_n: usize, q: usize, a: Vec<usize>, lrx: Vec<(usize, usize, usize)>) -> Vec<usize> {
     let mut a = a
         .into_iter()
         .enumerate()
         .map(|(i, ai)| (ai, i))
         .collect::<Vec<_>>();
     a.sort_unstable(); // 値が小さいほうから並び、同じ値ならindexが小さいほうが左になるようなソート
-    for _ in 0..q {
-        let (l, r, x) = reader.v3::<usize, usize, usize>();
-        writer.ln(a.lower_bound(&(x, r)) - a.lower_bound(&(x, l)));
-    }
+    (0..q)
+        .map(|i| {
+            let (l, r, x) = lrx[i];
+            a.lower_bound(&(x, r)) - a.lower_bound(&(x, l))
+        })
+        .collect()
 }
 
 #[test]
 fn test() {
-    let mut buf = Vec::new();
-    solve(
-        Reader::new(|| &b"5 3\n3 7 1 2 1\n1 5 1\n3 3 0\n0 4 3\n"[..]),
-        Writer::new(&mut buf),
+    assert_eq!(
+        vec![2, 0, 1],
+        solve(
+            5,
+            3,
+            vec![3, 7, 1, 2, 1],
+            vec![(1, 5, 1), (3, 3, 0), (0, 4, 3)]
+        )
     );
-    assert_eq!(Ok("2\n0\n1\n"), std::str::from_utf8(&buf));
 }
