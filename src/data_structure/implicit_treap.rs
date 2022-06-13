1b626c009a47f7f1e958583fc20318002e1b5b59
katand <katandps@gmail.com>
2022-06-14 01:15:56 +0900

    implicit treapをスニペットに追加

--- src/data_structure/implicit_treap.rs
+++ src/data_structure/implicit_treap.rs
@@ -4,10 +4,14 @@
 use crate::algo::xor_shift::XorShift;
 use crate::prelude::*;
 
+#[snippet(name = "implicit_treap", doc_hidden)]
+#[derive(Clone, Debug, Default)]
 pub struct ImplicitTreap<T> {
     randomizer: XorShift,
-    root: Box<TreapNode<T>>,
+    root: Box<implicit_treap_node::OptionalNode<T>>,
 }
+
+#[snippet(name = "implicit_treap", doc_hidden)]
 impl<T> ImplicitTreap<T> {
     /// # サイズ
     ///
@@ -26,6 +30,7 @@ impl<T> ImplicitTreap<T> {
     }
 }
 
+#[snippet(name = "implicit_treap", doc_hidden)]
 impl<T: PartialOrd + Default> ImplicitTreap<T> {
     /// # 挿入
     /// 先頭からpos(0-indexed)の位置にxを挿入
@@ -33,8 +38,10 @@ impl<T: PartialOrd + Default> ImplicitTreap<T> {
     /// ## 計算量
     /// $`O(logN)`$
     pub fn insert(&mut self, pos: usize, x: T) {
-        self.root
-            .insert(pos, TreapNode::new(x, self.randomizer.next().unwrap()))
+        self.root.insert(
+            pos,
+            implicit_treap_node::OptionalNode::new(x, self.randomizer.next().unwrap()),
+        )
     }
 
     /// # 削除
@@ -84,15 +91,7 @@ impl<T: PartialOrd + Default> ImplicitTreap<T> {
     }
 }
 
-impl<T> Default for ImplicitTreap<T> {
-    fn default() -> Self {
-        ImplicitTreap {
-            randomizer: XorShift::default(),
-            root: Box::new(TreapNode(None)),
-        }
-    }
-}
-
+#[snippet(name = "implicit_treap", doc_hidden)]
 impl<T> Index<usize> for ImplicitTreap<T> {
     type Output = T;
     fn index(&self, index: usize) -> &T {
@@ -100,21 +99,14 @@ impl<T> Index<usize> for ImplicitTreap<T> {
     }
 }
 
-impl<T: Clone> Clone for ImplicitTreap<T> {
-    fn clone(&self) -> Self {
-        Self {
-            randomizer: self.randomizer.clone(),
-            root: self.root.clone(),
-        }
-    }
-}
-
+#[snippet(name = "implicit_treap", doc_hidden)]
 impl<T: Display> Display for ImplicitTreap<T> {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         writeln!(f, "[{}]", self.root)
     }
 }
 
+#[snippet(name = "implicit_treap", doc_hidden)]
 impl<T: Clone + PartialOrd + Default> From<&[T]> for ImplicitTreap<T> {
     fn from(src: &[T]) -> Self {
         let mut ret = Self::default();
@@ -125,213 +117,189 @@ impl<T: Clone + PartialOrd + Default> From<&[T]> for ImplicitTreap<T> {
     }
 }
 
-#[derive(Default)]
-pub struct TreapNode<T>(Option<Node<T>>);
+#[snippet(name = "implicit_treap", doc_hidden)]
+mod implicit_treap_node {
+    use super::*;
 
-#[derive(Clone, Default)]
-struct Node<T> {
-    /// キー
-    key: T,
-    /// 優先度
-    p: u64,
-    /// 部分木のサイズ
-    size: usize,
-    /// 左右反転フラグ
-    rev: bool,
-    /// 左の子
-    l: Box<TreapNode<T>>,
-    /// 右の子
-    r: Box<TreapNode<T>>,
-}
+    #[derive(Clone, Debug, Default)]
+    pub struct OptionalNode<T>(Option<Node<T>>);
 
-impl<T> TreapNode<T> {
-    fn len(&self) -> usize {
-        self.0.as_ref().map_or(0, |node| node.size)
+    #[derive(Clone, Debug, Default)]
+    struct Node<T> {
+        /// キー
+        key: T,
+        /// 優先度
+        p: u64,
+        /// 部分木のサイズ
+        size: usize,
+        /// 左右反転フラグ
+        rev: bool,
+        /// 左の子
+        l: Box<OptionalNode<T>>,
+        /// 右の子
+        r: Box<OptionalNode<T>>,
     }
 
-    fn propagate_from_children(&mut self) {
-        if let Some(node) = self.0.as_mut() {
-            node.size = 1 + node.l.len() + node.r.len()
+    impl<T> OptionalNode<T> {
+        pub fn len(&self) -> usize {
+            self.0.as_ref().map_or(0, |node| node.size)
         }
-    }
 
-    fn propagate_to_children(&mut self) {
-        if let Some(node) = self.0.as_mut() {
-            if node.rev {
-                node.rev = false;
-                swap(&mut node.r, &mut node.l);
-                if let Some(r_node) = node.r.0.as_mut() {
-                    r_node.rev ^= true;
-                }
-                if let Some(l_node) = node.l.0.as_mut() {
-                    l_node.rev ^= true;
-                }
+        fn propagate_from_children(&mut self) {
+            if let Some(node) = self.0.as_mut() {
+                node.size = 1 + node.l.len() + node.r.len()
             }
         }
-        self.propagate_from_children()
-    }
-}
 
-impl<T: Default> TreapNode<T> {
-    fn new(key: T, p: u64) -> Self {
-        Self(Some(Node {
-            key,
-            p,
-            ..Default::default()
-        }))
-    }
-}
-
-impl<T: PartialOrd + Default> TreapNode<T> {
-    fn insert(&mut self, pos: usize, mut item: Self) {
-        let (mut l, mut r) = (Self::default(), Self::default());
-        self.split(pos, &mut l, &mut r);
-        self.merge(&mut l);
-        self.merge(&mut item);
-        self.merge(&mut r);
+        fn propagate_to_children(&mut self) {
+            if let Some(node) = self.0.as_mut() {
+                if node.rev {
+                    node.rev = false;
+                    swap(&mut node.r, &mut node.l);
+                    if let Some(r_node) = node.r.0.as_mut() {
+                        r_node.rev ^= true;
+                    }
+                    if let Some(l_node) = node.l.0.as_mut() {
+                        l_node.rev ^= true;
+                    }
+                }
+            }
+            self.propagate_from_children()
+        }
     }
 
-    fn erase(&mut self, pos: usize) -> Option<T> {
-        let (mut l, mut r) = (Self::default(), Self::default());
-        self.split(pos, &mut l, &mut r);
-        let (mut res, mut r2) = (Self::default(), Self::default());
-        r.split(1, &mut res, &mut r2);
-        self.merge(&mut l);
-        self.merge(&mut r2);
-        res.0.map(|node| node.key)
+    impl<T: Default> OptionalNode<T> {
+        pub fn new(key: T, p: u64) -> Self {
+            Self(Some(Node {
+                key,
+                p,
+                ..Default::default()
+            }))
+        }
     }
 
-    /// selfを l: $`[0, pos)`$ と r: $`[pos, n)`$ に分割する
-    fn split(&mut self, pos: usize, l: &mut Self, r: &mut Self) {
-        self.propagate_to_children();
-        r.propagate_to_children();
-        l.propagate_to_children();
-        if let Some(ref mut node) = self.0 {
-            let (mut l_temp, mut r_temp) = (Self::default(), Self::default());
-            if pos < node.l.len() + 1 {
-                // 左側の部分木を分割する 部分木の左側がl
-                node.l.split(pos, &mut l_temp, &mut r_temp);
-                swap(l, &mut l_temp);
-                swap(&mut node.l, &mut Box::new(r_temp));
-                swap(r, self);
-            } else {
-                // 右側の部分木を分割する
+    impl<T: PartialOrd + Default> OptionalNode<T> {
+        pub fn insert(&mut self, pos: usize, mut item: Self) {
+            let (mut l, mut r) = (Self::default(), Self::default());
+            self.split(pos, &mut l, &mut r);
+            self.merge(&mut l);
+            self.merge(&mut item);
+            self.merge(&mut r);
+        }
+
+        pub fn erase(&mut self, pos: usize) -> Option<T> {
+            let (mut l, mut r) = (Self::default(), Self::default());
+            self.split(pos, &mut l, &mut r);
+            let (mut res, mut r2) = (Self::default(), Self::default());
+            r.split(1, &mut res, &mut r2);
+            self.merge(&mut l);
+            self.merge(&mut r2);
+            res.0.map(|node| node.key)
+        }
+
+        /// selfを l: $`[0, pos)`$ と r: $`[pos, n)`$ に分割する
+        pub fn split(&mut self, pos: usize, l: &mut Self, r: &mut Self) {
+            self.propagate_to_children();
+            r.propagate_to_children();
+            l.propagate_to_children();
+            if let Some(ref mut node) = self.0 {
                 let (mut l_temp, mut r_temp) = (Self::default(), Self::default());
-                node.r
-                    .split(pos - node.l.len() - 1, &mut l_temp, &mut r_temp);
-                swap(r, &mut r_temp);
-                swap(&mut node.r, &mut Box::new(l_temp));
-                swap(l, self);
-            }
-        } else {
-            swap(l, &mut Self::default());
-            swap(r, &mut Self::default());
-        }
-        self.propagate_from_children();
-        l.propagate_from_children();
-        r.propagate_from_children();
-    }
-
-    // self の右に r をマージする
-    fn merge(&mut self, r: &mut Self) {
-        self.propagate_to_children();
-        r.propagate_to_children();
-        match (self.0.as_mut(), r.0.as_mut()) {
-            (Some(left_node), Some(right_node)) => {
-                if left_node.p > right_node.p {
-                    // 左の根のほうが優先度が大きいとき、左の木の右の子と右の木をマージする
-                    left_node.r.merge(r);
+                if pos < node.l.len() + 1 {
+                    // 左側の部分木を分割する 部分木の左側がl
+                    node.l.split(pos, &mut l_temp, &mut r_temp);
+                    swap(l, &mut l_temp);
+                    swap(&mut node.l, &mut Box::new(r_temp));
+                    swap(r, self);
                 } else {
-                    let mut temp = Self::default();
-                    swap(&mut temp, &mut right_node.l);
-                    self.merge(&mut temp);
-                    swap(self, &mut right_node.l);
-                    swap(self, r);
+                    // 右側の部分木を分割する
+                    let (mut l_temp, mut r_temp) = (Self::default(), Self::default());
+                    node.r
+                        .split(pos - node.l.len() - 1, &mut l_temp, &mut r_temp);
+                    swap(r, &mut r_temp);
+                    swap(&mut node.r, &mut Box::new(l_temp));
+                    swap(l, self);
                 }
+            } else {
+                swap(l, &mut Self::default());
+                swap(r, &mut Self::default());
             }
-            (Some(_), None) => (),
-            (None, Some(_)) => swap(self, r),
-            _ => (),
+            self.propagate_from_children();
+            l.propagate_from_children();
+            r.propagate_from_children();
         }
-        self.propagate_from_children();
-        r.propagate_from_children();
-    }
 
-    pub fn reverse(&mut self, l: usize, r: usize) {
-        // println!("{} {}", l, r);
-        let (mut l_tree, mut c_tree, mut r_tree, mut temp) = (
-            Self::default(),
-            Self::default(),
-            Self::default(),
-            Self::default(),
-        );
-        self.split(r, &mut temp, &mut r_tree);
-        temp.split(l, &mut l_tree, &mut c_tree);
-        if let Some(node) = c_tree.0.as_mut() {
-            node.rev ^= true;
+        // self の右に r をマージする
+        fn merge(&mut self, r: &mut Self) {
+            self.propagate_to_children();
+            r.propagate_to_children();
+            match (self.0.as_mut(), r.0.as_mut()) {
+                (Some(left_node), Some(right_node)) => {
+                    if left_node.p > right_node.p {
+                        // 左の根のほうが優先度が大きいとき、左の木の右の子と右の木をマージする
+                        left_node.r.merge(r);
+                    } else {
+                        let mut temp = Self::default();
+                        swap(&mut temp, &mut right_node.l);
+                        self.merge(&mut temp);
+                        swap(self, &mut right_node.l);
+                        swap(self, r);
+                    }
+                }
+                (Some(_), None) => (),
+                (None, Some(_)) => swap(self, r),
+                _ => (),
+            }
+            self.propagate_from_children();
+            r.propagate_from_children();
         }
-        self.merge(&mut l_tree);
-        self.merge(&mut c_tree);
-        self.merge(&mut r_tree);
-    }
 
-    pub fn rotate(&mut self, l: usize, r: usize, top: usize) {
-        // println!("{} {} {} {}", l, r, top, l + r - top);
-        self.reverse(l, r);
-        self.reverse(l, l + r - top);
-        self.reverse(l + r - top, r);
-    }
-}
+        pub fn reverse(&mut self, l: usize, r: usize) {
+            // println!("{} {}", l, r);
+            let (mut l_tree, mut c_tree, mut r_tree, mut temp) = (
+                Self::default(),
+                Self::default(),
+                Self::default(),
+                Self::default(),
+            );
+            self.split(r, &mut temp, &mut r_tree);
+            temp.split(l, &mut l_tree, &mut c_tree);
+            if let Some(node) = c_tree.0.as_mut() {
+                node.rev ^= true;
+            }
+            self.merge(&mut l_tree);
+            self.merge(&mut c_tree);
+            self.merge(&mut r_tree);
+        }
 
-impl<T> Index<usize> for TreapNode<T> {
-    type Output = T;
-    fn index(&self, index: usize) -> &T {
-        assert!(index < self.len());
-        self.0
-            .as_ref()
-            .map(|node| match () {
-                () if node.l.len() > index => node.l.index(index),
-                () if node.l.len() < index => node.r.index(index - node.l.len() - 1),
-                _ => &node.key,
-            })
-            .unwrap()
-    }
-}
-
-impl<T: Clone> Clone for TreapNode<T> {
-    fn clone(&self) -> Self {
-        match &self.0 {
-            Some(node) => Self(Some(Node {
-                key: node.key.clone(),
-                p: node.p,
-                size: node.size,
-                rev: node.rev,
-                l: node.l.clone(),
-                r: node.r.clone(),
-            })),
-            _ => Self(None),
+        pub fn rotate(&mut self, l: usize, r: usize, top: usize) {
+            // println!("{} {} {} {}", l, r, top, l + r - top);
+            self.reverse(l, r);
+            self.reverse(l, l + r - top);
+            self.reverse(l + r - top, r);
         }
     }
-}
 
-impl<T: Display> Display for TreapNode<T> {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        match &self.0 {
-            Some(node) => write!(f, "{} {} {}", node.l, node.key, node.r),
-            _ => write!(f, ""),
+    impl<T> Index<usize> for OptionalNode<T> {
+        type Output = T;
+        fn index(&self, index: usize) -> &T {
+            assert!(index < self.len());
+            self.0
+                .as_ref()
+                .map(|node| match () {
+                    () if node.l.len() > index => node.l.index(index),
+                    () if node.l.len() < index => node.r.index(index - node.l.len() - 1),
+                    _ => &node.key,
+                })
+                .unwrap()
         }
     }
-}
 
-impl<T: Display> Debug for TreapNode<T> {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        match &self.0 {
-            Some(node) => write!(
-                f,
-                "key{}size{}:[l:{:?} r:{:?}]",
-                node.key, node.size, node.l, node.r
-            ),
-            _ => write!(f, ""),
+    impl<T: Display> Display for OptionalNode<T> {
+        fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+            match &self.0 {
+                Some(node) => write!(f, "{} {} {}", node.l, node.key, node.r),
+                _ => write!(f, ""),
+            }
         }
     }
 }
