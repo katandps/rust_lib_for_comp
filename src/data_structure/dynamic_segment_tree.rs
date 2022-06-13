e7d1a11ad90c9b5d6a4f79ffb2d5832ac1baded6
katand <katandps@gmail.com>
2022-06-13 20:12:29 +0900

    dynamic segment treeのノードをBoxで持つように変更

--- src/data_structure/dynamic_segment_tree.rs
+++ src/data_structure/dynamic_segment_tree.rs
@@ -2,21 +2,20 @@
 //! セグメント木よりメモリアクセスが遅いが、メモリ使用量は挿入したノードの数を上界とする。
 //! データの挿入が$`O( \log N)`$となっていることに注意。
 //! ## verify
-//! [ARC008D](https://atcoder.jp/contests/arc008/submissions/26669109)
-use crate::algebra::Monoid;
+//! [ARC008D](https://atcoder.jp/contests/arc008/submissions/32453987)
 use crate::prelude::*;
 
 #[snippet(name = "dynamic-segment-tree", doc_hidden)]
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct DynamicSegmentTree<M: Monoid> {
-    root: node::Node<M>,
+    root: dynamic_segment_tree_node::OptionalNode<M>,
 }
 
 #[snippet(name = "dynamic-segment-tree", doc_hidden)]
 impl<M: Monoid> Default for DynamicSegmentTree<M> {
     fn default() -> Self {
         Self {
-            root: node::Node::<M>::default(),
+            root: dynamic_segment_tree_node::OptionalNode::default(),
         }
     }
 }
@@ -24,7 +23,8 @@ impl<M: Monoid> Default for DynamicSegmentTree<M> {
 #[snippet(name = "dynamic-segment-tree", doc_hidden)]
 impl<M: Monoid> DynamicSegmentTree<M> {
     /// 最大幅を $`2^{BIT_LEN}`$ とする
-    pub const BIT_LEN: i32 = 62;
+    const BIT_LEN: i32 = 62;
+    const MAX: u64 = 1 << Self::BIT_LEN;
     /// 値iをvalueに更新する
     /// ## 計算量
     /// $`O( \log N)`$
@@ -40,101 +40,88 @@ impl<M: Monoid> DynamicSegmentTree<M> {
     /// Rangeで与えられた区間の値を取得する
     /// ## 計算量
     /// $`O( \log N)`$
-    pub fn prod<R>(&self, range: R) -> M::M
-    where
-        R: RangeBounds<usize>,
-    {
-        let (l, r) = Self::make_lr(range);
-        self.root.prod(l, r, 0, 1 << Self::BIT_LEN)
-    }
-
-    /// Range to [l, r)
-    fn make_lr<R: RangeBounds<usize>>(range: R) -> (u64, u64) {
-        use Bound::*;
-        let l = match range.start_bound() {
-            Unbounded => 0,
-            Included(&s) => s,
-            Excluded(&s) => s + 1,
-        };
-        let r = match range.end_bound() {
-            Unbounded => 1 << Self::BIT_LEN,
-            Included(&e) => e + 1,
-            Excluded(&e) => e,
-        };
-        (l as u64, r as u64)
+    pub fn prod<R: RangeBounds<u64>>(&self, range: R) -> M::M {
+        let (l, r) = to_lr(&range, Self::MAX);
+        self.root.prod(l, r, 0, Self::MAX)
     }
 }
 
 #[snippet(name = "dynamic-segment-tree", doc_hidden)]
-mod node {
-    use super::Monoid;
+mod dynamic_segment_tree_node {
+    use super::*;
     #[derive(Clone, Debug)]
-    pub struct Node<M: Monoid> {
-        value: M::M,
-        child: Vec<Option<Node<M>>>,
-    }
+    pub struct OptionalNode<M: Monoid>(Option<Node<M>>);
 
-    impl<M: Monoid> Default for Node<M> {
+    impl<M: Monoid> Default for OptionalNode<M> {
         fn default() -> Self {
-            Self {
-                value: M::unit(),
-                child: vec![None, None],
-            }
+            Self(None)
         }
     }
 
+    #[derive(Clone, Debug)]
+    struct Node<M: Monoid> {
+        value: M::M,
+        l: Box<OptionalNode<M>>,
+        r: Box<OptionalNode<M>>,
+    }
+
+    impl<M: Monoid> OptionalNode<M> {
+        pub fn new(value: M::M) -> Self {
+            Self(Some(Node {
+                value,
+                l: Box::new(Self(None)),
+                r: Box::new(Self(None)),
+            }))
+        }
+
+        pub fn set(&mut self, idx: u64, bit: i32, value: M::M) {
+            match self.0.as_mut() {
+                Some(node) => {
+                    node.child_mut(idx, bit).set(idx, bit - 1, value);
+                    node.value = M::op(
+                        &node.l.prod(0, 1 << 62, 0, 1 << bit),
+                        &node.r.prod(0, 1 << 62, 0, 1 << bit),
+                    )
+                }
+                None if bit < 0 => swap(self, &mut Self::new(value)),
+                None => {
+                    swap(self, &mut Self::new(value.clone()));
+                    self.set(idx, bit, value);
+                }
+            }
+        }
+
+        pub fn get(&self, idx: u64, bit: i32) -> M::M {
+            match &self.0 {
+                Some(node) if bit < 0 => node.value.clone(),
+                Some(node) => node.child(idx, bit).get(idx, bit - 1),
+                None => M::unit(),
+            }
+        }
+
+        /// [l, r)のうち、[lb, ub)の内部にあるものをprodして返す
+        pub fn prod(&self, l: u64, r: u64, lb: u64, ub: u64) -> M::M {
+            match &self.0 {
+                Some(node) if l <= lb && ub <= r => node.value.clone(),
+                Some(node) if lb < r && l < ub => M::op(
+                    &node.l.prod(l, r, lb, (lb + ub) >> 1),
+                    &node.r.prod(l, r, (lb + ub) >> 1, ub),
+                ),
+                _ => M::unit(),
+            }
+        }
+    }
     impl<M: Monoid> Node<M> {
-        pub fn set(&mut self, pos: u64, bit: i32, value: M::M) {
-            if bit < 0 {
-                self.value = value;
-                return;
-            }
-            let dst = (pos >> bit & 1) as usize;
-            if let Some(c) = self.child[dst].as_mut() {
-                c.set(pos, bit - 1, value);
-            } else {
-                let mut node = Node::default();
-                node.set(pos, bit - 1, value);
-                self.child[dst] = Some(node);
-            }
-            self.value = M::op(
-                &self.child[0]
-                    .as_ref()
-                    .map_or(M::unit(), |c| c.value.clone()),
-                &self.child[1]
-                    .as_ref()
-                    .map_or(M::unit(), |c| c.value.clone()),
-            );
-        }
-
-        pub fn get(&self, pos: u64, bit: i32) -> M::M {
-            if bit < 0 {
-                return self.value.clone();
-            }
-            let dst = (pos >> bit & 1) as usize;
-            if let Some(c) = &self.child[dst] {
-                c.get(pos, bit - 1)
-            } else {
-                M::unit()
+        fn child_mut(&mut self, idx: u64, bit: i32) -> &mut OptionalNode<M> {
+            match () {
+                () if idx >> bit & 1 == 0 => self.l.as_mut(),
+                _ => self.r.as_mut(),
             }
         }
-
-        /// [left, right)のうち、[lower_bound, upper_bound)の内部にあるものをprodして返す
-        pub fn prod(&self, left: u64, right: u64, lower_bound: u64, upper_bound: u64) -> M::M {
-            if right <= lower_bound || upper_bound <= left {
-                M::unit()
-            } else if left <= lower_bound && upper_bound <= right {
-                self.value.clone()
-            } else {
-                let mid = (lower_bound + upper_bound) >> 1;
-                M::op(
-                    &self.child[0]
-                        .as_ref()
-                        .map_or(M::unit(), |c| c.prod(left, right, lower_bound, mid)),
-                    &self.child[1]
-                        .as_ref()
-                        .map_or(M::unit(), |c| c.prod(left, right, mid, upper_bound)),
-                )
+        fn child(&self, idx: u64, bit: i32) -> &OptionalNode<M> {
+            match () {
+                () if idx >> bit & 1 == 0 => &self.l,
+                _ => &self.r,
             }
         }
     }
@@ -149,7 +136,10 @@ mod test {
     #[test]
     fn test() {
         let mut segtree = DynamicSegmentTree::<Maximization<i64>>::default();
+
         segtree.set(50000000, 8);
+
+        assert_eq!(8, segtree.get(50000000));
         segtree.set(80000000000000, 10);
         assert_eq!(Maximization::<i64>::unit(), segtree.prod(0..50000000));
         assert_eq!(8, segtree.prod(0..50000001));
@@ -161,10 +151,10 @@ mod test {
         );
         assert_eq!(10, segtree.prod(50000001..80000000000001));
         assert_eq!(10, segtree.prod(80000000000000..80000000000001));
-        assert_eq!(10, segtree.prod(80000000000000..10000000000000000000));
+        assert_eq!(10, segtree.prod(80000000000000..100000000000000000));
         assert_eq!(
             Maximization::<i64>::unit(),
-            segtree.prod(80000000000001..10000000000000000000)
+            segtree.prod(80000000000001..100000000000000000)
         );
     }
 }
