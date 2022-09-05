1806f0977fa2e0edf5bf25a5e092ef3372d40706
katand <katandps@gmail.com>
2022-09-05 11:03:32 +0900

    ベンチマークのファイルを追加

---
+++ benches/benchmark.rs
@@ -0,0 +1,19 @@
+// use criterion::{criterion_group, criterion_main, Criterion};
+
+// use rust_lib_for_comp::comp_answer;
+// use rust_lib_for_comp::my_answer;
+
+// fn bm1(c: &mut Criterion) {
+//     c.bench_function("My Answer", |b| b.iter(|| my_answer::pascals_triangle(10)));
+// }
+
+// fn bm2(c: &mut Criterion) {
+//     c.bench_function("Comp Answer", |b| {
+//         b.iter(|| comp_answer::pascals_triangle(10))
+//     });
+// }
+
+// criterion_group!(benches, bm1, bm2);
+// criterion_main!(benches);
+
+fn main() {}
