c287a502c29045957a84a3db1c91bdb5321df868
katand <katandps@gmail.com>
2022-09-08 01:32:36 +0900

    木に対するメソッドをTrait化

--- src/graph/tree.rs
+++ src/graph/tree.rs
@@ -1,4 +1,5 @@
 //! # 木
+//! グラフが木の場合に使えるアルゴリズム
 
 use crate::graph::GraphTrait;
 use crate::prelude::*;
@@ -7,33 +8,68 @@ pub mod euler_tour;
 pub mod heavy_light_decomposition;
 pub mod lowest_common_ancestor;
 
-/// # 頂点の高さ
-/// 適当な頂点を根としたときの頂点の高さを返す
-///
-/// ## todo
-/// 直径からの高さを求めるべき
-///
-/// ## verify
-/// [ABC233F](https://atcoder.jp/contests/abc233/submissions/28183153)
-#[snippet(name = "tree-graph", doc_hidden)]
-pub fn rank<G: GraphTrait>(g: &G) -> Vec<i64> {
-    let mut rank = vec![None; g.size()];
-    for i in 0..g.size() {
-        if rank[i].is_none() {
-            rank[i] = Some(0);
-            rank_dfs(i, i, g, &mut rank);
-        }
-    }
-    rank.into_iter().flatten().collect()
-}
+#[snippet(name = "min_max", doc_hidden)]
+#[allow(unused_macros)]
+macro_rules! chmax {($base:expr, $($cmps:expr),+ $(,)*) => {{let cmp_max = max!($($cmps),+);if $base < cmp_max {$base = cmp_max;true} else {false}}};}
+
+#[snippet(name = "min_max", doc_hidden)]
+#[allow(unused_macros)]
+macro_rules! max {($a:expr $(,)*) => {{$a}};($a:expr, $b:expr $(,)*) => {{if $a > $b {$a} else {$b}}};($a:expr, $($rest:expr),+ $(,)*) => {{let b = max!($($rest),+);if $a > b {$a} else {b}}};}
 
 #[snippet(name = "tree-graph", doc_hidden)]
-fn rank_dfs<G: GraphTrait>(cur: usize, par: usize, g: &G, rank: &mut Vec<Option<i64>>) {
-    for (dst, _weight) in g.edges(cur) {
-        if dst == par {
-            continue;
+pub trait TreeGraph: GraphTrait {
+    /// # 頂点の高さ
+    /// 適当な頂点を根としたときの頂点の高さを返す
+    ///
+    /// ## todo
+    /// 直径からの高さを求めるべき
+    ///
+    /// ## verify
+    /// [ABC233F](https://atcoder.jp/contests/abc233/submissions/28183153)
+    fn rank(&self) -> Vec<i64> {
+        let mut rank = vec![None; self.size()];
+        for i in 0..self.size() {
+            if rank[i].is_none() {
+                rank[i] = Some(0);
+                self.rank_dfs(i, i, &mut rank);
+            }
         }
-        rank[dst] = rank[cur].map(|k| k + 1);
-        rank_dfs(dst, cur, g, rank);
+        rank.into_iter().flatten().collect()
+    }
+    fn rank_dfs(&self, cur: usize, par: usize, rank: &mut Vec<Option<i64>>) {
+        for (dst, _weight) in self.edges(cur) {
+            if dst == par {
+                continue;
+            }
+            rank[dst] = rank[cur].map(|k| k + 1);
+            self.rank_dfs(dst, cur, rank);
+        }
+    }
+    /// # いくつかの辺を使って到達できる点のうち、最も遠い点を求める
+    ///
+    /// l - rが直径
+    /// ```
+    /// use rust_lib_for_comp::graph::adjacency_list::Graph;
+    /// use crate::rust_lib_for_comp::graph::tree::TreeGraph;
+    /// let graph = Graph::<i32>::new(5);
+    /// let l = graph.farthest_vertex(0);
+    /// let r = graph.farthest_vertex(l);
+    /// ```
+    fn farthest_vertex(&self, src: usize) -> usize {
+        let mut rank = vec![None; self.size()];
+        rank[src] = Some(0);
+        self.rank_dfs(src, src, &mut rank);
+        let mut dist = -1;
+        let mut ret = 0;
+        for (i, rank) in rank.iter().enumerate() {
+            if let Some(d) = rank {
+                if chmax!(dist, *d) {
+                    ret = i;
+                }
+            }
+        }
+        ret
     }
 }
+#[snippet(name = "tree-graph", doc_hidden)]
+impl<G: GraphTrait> TreeGraph for G {}
