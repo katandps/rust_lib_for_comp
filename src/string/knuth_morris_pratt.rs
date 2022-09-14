958048e08106fdd0da4a730bdaabb92c7b3d1ba9
katand <katandps@gmail.com>
2022-09-14 20:02:15 +0900

    KMP法のバグを修正/検索を実装

--- src/string/knuth_morris_pratt.rs
+++ src/string/knuth_morris_pratt.rs
@@ -5,34 +5,50 @@
 //! 接頭辞Pのtagged borderとは、Pのborder Wのうち、P[|W|] != 残りの文字列\[0\] であるもの
 //! この値をならし計算量$O(N)$ですべて求める。
 //!
+//! ## verify
+//! [ABC150F](https://atcoder.jp/contests/abc150/submissions/34853947)
 use crate::prelude::*;
 
 #[snippet(name = "knuth-morris-pratt", doc_hidden)]
 pub fn tagged_border<T: PartialEq>(src: &[T]) -> Vec<i64> {
-    let mut ret = vec![0; src.len() + 1];
+    let n = src.len();
+    let (mut ret, mut j) = (vec![0; n + 1], -1);
     ret[0] = -1;
-    let mut j = -1;
-    for i in 0..src.len() {
+    for i in 0..n {
         while j >= 0 && src[i] != src[j as usize] {
             j = ret[j as usize]
         }
         j += 1;
-        if i + 1 < src.len() && src[i + 1] == src[j as usize] {
-            ret[i + 1] = ret[j as usize]
+        ret[i + 1] = j;
+    }
+    ret
+}
+
+#[snippet(name = "knuth-morris-pratt", doc_hidden)]
+pub fn search<T: PartialEq>(src: &[T], word: &[T]) -> Vec<i64> {
+    let table = tagged_border(word);
+    let (mut m, mut i, n, mut ret) = (0, 0, src.len() as i64, Vec::new());
+    while m + i < n {
+        if word[i as usize] == src[(m + i) as usize] {
+            i += 1;
+            if i == word.len() as i64 {
+                ret.push(m);
+                m = m + i - table[i as usize];
+                i = table[i as usize];
+            }
         } else {
-            ret[i + 1] = j;
+            m = m + i - table[i as usize];
+            if i > 0 {
+                i = table[i as usize]
+            }
         }
     }
     ret
 }
 
-pub fn search<T: PartialEq>(_src: &[T], _word: &[T]) {
-    todo!()
-}
-
 #[test]
 fn test_kmp() {
-    let src = "GCAGAGAG".chars().collect::<Vec<_>>();
-    let ret = tagged_border(&src);
-    assert_eq!(ret, vec![-1, 0, 0, -1, 1, -1, 1, -1, 1]);
+    let src = "ABCDEABCABAAABC".chars().collect::<Vec<_>>();
+    let word = "ABC".chars().collect::<Vec<_>>();
+    assert_eq!(vec![0, 5, 12], search(&src, &word));
 }
