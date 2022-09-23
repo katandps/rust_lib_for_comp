7ab2c495ffd48320c658dc2413e97a443a992961
katand <katandps@gmail.com>
2022-09-23 20:33:50 +0900

    トポロジカルソートをTrait化

--- src/graph/directed_acyclic_graph.rs
+++ src/graph/directed_acyclic_graph.rs
@@ -5,10 +5,8 @@ use crate::graph::GraphTrait;
 use crate::prelude::*;
 
 #[snippet(name = "directed-acyclic-graph", doc_hidden)]
-pub struct Dag;
 
-#[snippet(name = "directed-acyclic-graph", doc_hidden)]
-impl Dag {
+pub trait Dag {
     /// # トポロジカルソート
     /// DAGをトポロジカルソートし、結果の頂点列を返す
     ///
@@ -17,11 +15,17 @@ impl Dag {
     ///
     /// ## 備考
     /// DAGでない場合はグラフの頂点数と頂点列のサイズが一致しない
-    pub fn topological_sort<W, G>(g: &G) -> Vec<usize>
-    where
-        G: GraphTrait<Weight = W>,
-    {
-        let mut deg = g.indegree();
+    fn topological_sort(&self) -> Vec<usize>;
+
+    /// ## 経路の数え上げ
+    /// lを始点とする各点までの経路数をDPで求める
+    fn path(&self, l: usize) -> Vec<usize>;
+}
+
+#[snippet(name = "directed-acyclic-graph", doc_hidden)]
+impl<G: GraphTrait> Dag for G {
+    fn topological_sort(&self) -> Vec<usize> {
+        let mut deg = self.indegree();
 
         let mut q = VecDeque::new();
         deg.iter().enumerate().for_each(|(i, deg)| {
@@ -32,7 +36,7 @@ impl Dag {
 
         let mut ret = Vec::new();
         while let Some(src) = q.pop_front() {
-            g.edges(src).into_iter().for_each(|(dst, _weight)| {
+            self.edges(src).into_iter().for_each(|(dst, _weight)| {
                 deg[dst] -= 1;
                 if deg[dst] == 0 {
                     q.push_back(dst)
@@ -43,17 +47,12 @@ impl Dag {
         ret
     }
 
-    /// ## 経路の数え上げ
-    /// lを始点とする各点までの経路数をDPで求める
-    pub fn path<W, G>(g: &G, l: usize) -> Vec<usize>
-    where
-        G: GraphTrait<Weight = W>,
-    {
-        let list = Self::topological_sort(g);
-        let mut dp = vec![0; g.size()];
+    fn path(&self, l: usize) -> Vec<usize> {
+        let list = self.topological_sort();
+        let mut dp = vec![0; self.size()];
         dp[l] = 1;
         for src in list {
-            for (dst, _weight) in g.edges(src) {
+            for (dst, _weight) in self.edges(src) {
                 dp[dst] += dp[src];
             }
         }
