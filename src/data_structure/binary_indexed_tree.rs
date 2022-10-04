2da1d8592134482b3cf52eb9785acf3b81967d28
katand <katandps@gmail.com>
2022-10-04 20:17:27 +0900

    BITにupdate()を追加 実装をmodに閉じ込める

--- src/data_structure/binary_indexed_tree.rs
+++ src/data_structure/binary_indexed_tree.rs
@@ -12,126 +12,137 @@ pub struct BinaryIndexedTree<A: Magma> {
 }
 
 #[snippet(name = "binary-indexed-tree", doc_hidden)]
-/// サイズを指定して作成する
-impl<A: AbelianGroup> From<usize> for BinaryIndexedTree<A> {
-    fn from(length: usize) -> Self {
-        Self {
-            n: length,
-            bit: vec![A::unit(); length + 1],
-        }
-    }
-}
+mod binary_indexed_tree_impl {
+    use super::{AbelianGroup, BinaryIndexedTree, Debug, Formatter, RangeBounds, ToLR};
 
-/// ソースを指定して作成する
-#[snippet(name = "binary-indexed-tree", doc_hidden)]
-impl<A: AbelianGroup> From<&[A::M]> for BinaryIndexedTree<A> {
-    fn from(src: &[A::M]) -> Self {
-        let mut bit = Self::from(src.len());
-        src.iter()
-            .enumerate()
-            .for_each(|(i, item)| bit.add(i + 1, item.clone()));
-        bit
-    }
-}
-
-#[snippet(name = "binary-indexed-tree", doc_hidden)]
-impl<A: AbelianGroup> BinaryIndexedTree<A> {
-    /// add $x$ to $i$
-    pub fn add(&mut self, i: usize, x: A::M) {
-        let mut idx = i as i32 + 1;
-        while idx <= self.n as i32 {
-            self.bit[idx as usize] = A::op(&self.bit[idx as usize], &x);
-            idx += idx & -idx;
-        }
-    }
-
-    /// sum of $[0, i)$
-    pub fn sum(&self, i: usize) -> A::M {
-        let mut ret = A::unit();
-        let mut idx = i as i32 + 1;
-        while idx > 0 {
-            ret = A::op(&ret, &self.bit[idx as usize]);
-            idx -= idx & -idx;
-        }
-        ret
-    }
-
-    /// sum of range
-    pub fn fold<R: RangeBounds<usize>>(&self, range: R) -> A::M {
-        let (a, b) = range.to_lr();
-        if b == 0 {
-            A::unit()
-        } else if a == 0 {
-            self.sum(b - 1)
-        } else {
-            A::op(&self.sum(b - 1), &A::inv(&self.sum(a - 1)))
-        }
-    }
-
-    /// sumが初めてlbを超える位置を返す
-    /// 二項演算の結果が単調増加するときに使える
-    pub fn lower_bound(&self, lb: A::M) -> usize
-    where
-        A::M: PartialOrd,
-    {
-        if lb <= A::unit() {
-            return 0;
-        }
-        let (mut ret, mut len) = (0, 1);
-        while len < self.n {
-            len <<= 1;
-        }
-        let mut sum = A::unit();
-        while len > 0 {
-            if ret + len <= self.n && A::op(&sum, &self.bit[ret + len]) < lb {
-                sum = A::op(&sum, &self.bit[ret + len]);
-                ret += len;
+    /// サイズを指定して作成する
+    impl<A: AbelianGroup> From<usize> for BinaryIndexedTree<A> {
+        fn from(length: usize) -> Self {
+            Self {
+                n: length,
+                bit: vec![A::unit(); length + 1],
             }
-            len >>= 1;
         }
-        ret
     }
 
-    /// sumが初めてubを下回る位置を返す
-    /// 二項演算の結果が単調減少するときに使える
-    pub fn upper_bound(&self, ub: A::M) -> usize
-    where
-        A::M: PartialOrd,
-    {
-        if ub >= A::unit() {
-            return 0;
+    /// ソースを指定して作成する
+    impl<A: AbelianGroup> From<&[A::M]> for BinaryIndexedTree<A> {
+        fn from(src: &[A::M]) -> Self {
+            let mut bit = Self::from(src.len());
+            src.iter()
+                .enumerate()
+                .for_each(|(i, item)| bit.add(i + 1, item.clone()));
+            bit
         }
-        let (mut ret, mut len) = (0, 1);
-        while len < self.n {
-            len <<= 1;
-        }
-        let mut sum = A::unit();
-        while len > 0 {
-            if ret + len <= self.n && A::op(&sum, &self.bit[ret + len]) > ub {
-                sum = A::op(&sum, &self.bit[ret + len]);
-                ret += len;
+    }
+
+    impl<A: AbelianGroup> BinaryIndexedTree<A> {
+        /// add $x$ to $i$
+        pub fn add(&mut self, i: usize, x: A::M) {
+            let mut idx = i as i32 + 1;
+            while idx <= self.n as i32 {
+                self.bit[idx as usize] = A::op(&self.bit[idx as usize], &x);
+                idx += idx & -idx;
             }
-            len >>= 1;
         }
-        ret
-    }
-}
 
-#[snippet(name = "binary-indexed-tree", doc_hidden)]
-impl<A: AbelianGroup> Debug for BinaryIndexedTree<A>
-where
-    A::M: Debug,
-{
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        let v = (0..self.n)
-            .map(|i| format!("{:?}", self.fold(i..=i)))
-            .collect::<Vec<_>>()
-            .join(" ");
-        let v2 = (0..self.n)
-            .map(|i| format!("{:?}", self.sum(i)))
-            .collect::<Vec<_>>()
-            .join(" ");
-        write!(f, "\n{}\n{}", v, v2)
+        /// update $i$ to $x$
+        pub fn update(&mut self, i: usize, mut x: A::M) {
+            let mut idx = i as i32 + 1;
+            x = A::op(&x, &A::inv(&self.fold(i..=i)));
+            while idx <= self.n as i32 {
+                self.bit[idx as usize] = A::op(&self.bit[idx as usize], &x);
+                idx += idx & -idx;
+            }
+        }
+
+        /// sum of $[0, i)$
+        pub fn sum(&self, i: usize) -> A::M {
+            let mut ret = A::unit();
+            let mut idx = i as i32 + 1;
+            while idx > 0 {
+                ret = A::op(&ret, &self.bit[idx as usize]);
+                idx -= idx & -idx;
+            }
+            ret
+        }
+
+        /// sum of range
+        pub fn fold<R: RangeBounds<usize>>(&self, range: R) -> A::M {
+            let (a, b) = range.to_lr();
+            if b == 0 {
+                A::unit()
+            } else if a == 0 {
+                self.sum(b - 1)
+            } else {
+                A::op(&self.sum(b - 1), &A::inv(&self.sum(a - 1)))
+            }
+        }
+
+        /// sumが初めてlbを超える位置を返す
+        /// 二項演算の結果が単調増加するときに使える
+        pub fn lower_bound(&self, lb: A::M) -> usize
+        where
+            A::M: PartialOrd,
+        {
+            if lb <= A::unit() {
+                return 0;
+            }
+            let (mut ret, mut len) = (0, 1);
+            while len < self.n {
+                len <<= 1;
+            }
+            let mut sum = A::unit();
+            while len > 0 {
+                if ret + len <= self.n && A::op(&sum, &self.bit[ret + len]) < lb {
+                    sum = A::op(&sum, &self.bit[ret + len]);
+                    ret += len;
+                }
+                len >>= 1;
+            }
+            ret
+        }
+
+        /// sumが初めてubを下回る位置を返す
+        /// 二項演算の結果が単調減少するときに使える
+        pub fn upper_bound(&self, ub: A::M) -> usize
+        where
+            A::M: PartialOrd,
+        {
+            if ub >= A::unit() {
+                return 0;
+            }
+            let (mut ret, mut len) = (0, 1);
+            while len < self.n {
+                len <<= 1;
+            }
+            let mut sum = A::unit();
+            while len > 0 {
+                if ret + len <= self.n && A::op(&sum, &self.bit[ret + len]) > ub {
+                    sum = A::op(&sum, &self.bit[ret + len]);
+                    ret += len;
+                }
+                len >>= 1;
+            }
+            ret
+        }
+    }
+
+    impl<A: AbelianGroup> Debug for BinaryIndexedTree<A>
+    where
+        A::M: Debug,
+    {
+        fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+            let v = (0..self.n)
+                .map(|i| format!("{:?}", self.fold(i..=i)))
+                .collect::<Vec<_>>()
+                .join(" ");
+            let v2 = (0..self.n)
+                .map(|i| format!("{:?}", self.sum(i)))
+                .collect::<Vec<_>>()
+                .join(" ");
+            write!(f, "\n{}\n{}", v, v2)
+        }
     }
 }
 
