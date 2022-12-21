a504056b575b8d0f1eee6e62312d9b0b2df1ab5e
katand <katandps@gmail.com>
2022-12-21 12:15:15 +0900

    スライド区間の総和に機能/テストを追加

--- src/data_structure/priority_sum.rs
+++ src/data_structure/priority_sum.rs
@@ -1,6 +1,5 @@
 //! # 集合の上位k個の総和
-//! Cmpを実装している集合の上位k個について、アーベル群の演算結果を求める
-//! 集合の操作を高速に変更できる
+//! アーベル群である順序集合の上位k個について、総和を求める
 
 use crate::prelude::*;
 
@@ -8,7 +7,7 @@ use crate::prelude::*;
 pub use priority_sum_impl::PrioritySum;
 #[snippet(name = "priority-sum", doc_hidden)]
 mod priority_sum_impl {
-    use super::{AbelianGroup, BinaryHeap, Reverse};
+    use super::{AbelianGroup, BinaryHeap, Ordering, Reverse};
 
     #[derive(Debug, Clone, Default)]
     pub struct PrioritySum<M: AbelianGroup>
@@ -27,6 +26,10 @@ mod priority_sum_impl {
     where
         M::M: Ord,
     {
+        /// # $k$で初期化
+        ///
+        /// ## 計算量
+        /// $O(1)$
         pub fn new(k: usize) -> Self {
             Self {
                 sum: M::unit(),
@@ -38,34 +41,73 @@ mod priority_sum_impl {
             }
         }
 
+        /// # 集合に$x$を追加
+        ///
+        /// ## 計算量
+        /// ならし$O(\logN)$ ($N$は集合の要素数)
         pub fn add(&mut self, x: M::M) {
             self.sum = M::op(&self.sum, &x);
             self.in_v.push(x);
             self.modify();
         }
 
+        /// # 集合から$x$を削除
+        ///
+        /// ## 計算量
+        /// ならし$O(\logN)$ ($N$は集合の要素数)
         pub fn erase(&mut self, x: M::M) {
             if let Some(xd) = self.in_v.pop() {
-                if x == xd {
-                    self.sum = M::op(&self.sum, &M::inv(&x));
-                    self.modify();
-                    return;
+                match x.cmp(&xd) {
+                    Ordering::Equal => self.sum = M::op(&self.sum, &M::inv(&x)),
+                    Ordering::Greater => {
+                        self.in_v.push(xd);
+                        self.d_out.push(Reverse(x));
+                    }
+                    Ordering::Less => {
+                        self.sum = M::op(&self.sum, &M::inv(&x));
+                        self.in_v.push(xd);
+                        self.d_in.push(x);
+                    }
                 }
-                if xd > x {
-                    self.sum = M::op(&self.sum, &M::inv(&x));
-                    self.d_in.push(x);
-                } else {
-                    self.d_out.push(Reverse(x));
-                }
-                self.in_v.push(xd);
             } else {
                 self.d_out.push(Reverse(x));
             }
             self.modify();
         }
+
+        /// # 総和を取得
+        ///
+        /// ## 計算量
+        /// $O(1)$
         pub fn query(&self) -> M::M {
             self.sum.clone()
         }
+
+        /// # 集合の個数を取得
+        ///
+        /// ## 計算量
+        /// $O(1)$
+        pub fn len(&self) -> usize {
+            self.in_v.len() + self.out_v.len() - self.d_in.len() - self.d_out.len()
+        }
+
+        /// # $k$の更新
+        ///
+        /// ## 計算量
+        /// $O((kの変更量)*\logN) ($N$は集合の要素数)
+        pub fn set_k(&mut self, k: usize) {
+            self.k = k;
+            self.modify()
+        }
+
+        /// # 現在の$k$を取得
+        ///
+        /// ## 計算量
+        /// $O(1)$
+        pub fn k(&self) -> usize {
+            self.k
+        }
+
         fn modify(&mut self) {
             while self.in_v.len() < self.k + self.d_in.len() && !self.out_v.is_empty() {
                 if let Some(p) = self.out_v.pop() {
@@ -108,5 +150,24 @@ mod priority_sum_impl {
                 }
             }
         }
+    }
+}
+
+#[test]
+fn test() {
+    use crate::algebra::binary_operation::addition::Addition;
+    use crate::algo::xor_shift::XorShift;
+    let mut xorshift = XorShift::default();
+
+    let k = 50;
+    let mut v = Vec::new();
+    let mut ps = PrioritySum::<Addition<i64>>::new(k);
+    for _ in 0..1000 {
+        let ai = xorshift.rand_range(0..100000);
+        v.push(ai);
+        ps.add(ai);
+        v.sort();
+        let sum = v.iter().take(k).sum::<i64>();
+        assert_eq!(sum, ps.query());
     }
 }
