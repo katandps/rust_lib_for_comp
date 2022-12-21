b9544b90d78de1391c09380f1793f0bc546d4264
yamashita sho <katandps@gmail.com>
2022-12-21 10:08:54 +0900

    trieの型を固定しない

--- src/string/trie.rs
+++ src/string/trie.rs
@@ -4,13 +4,13 @@
 use crate::prelude::*;
 
 #[derive(Clone, Debug, Default)]
-pub struct Trie<T> {
-    childs: HashMap<char, Trie<T>>,
+pub struct Trie<K: Hash, T> {
+    childs: HashMap<K, Trie<K, T>>,
     value: Option<T>,
 }
 
-impl<T: Default> Trie<T> {
-    pub fn insert(&mut self, key: &[char], value: T) {
+impl<K: Copy + Hash + Eq + Default, T: Default> Trie<K, T> {
+    pub fn insert(&mut self, key: &[K], value: T) {
         match key.first() {
             Some(c) => self
                 .childs
@@ -21,10 +21,20 @@ impl<T: Default> Trie<T> {
         }
     }
 
-    pub fn get(&self, key: &[char]) -> Option<&T> {
+    pub fn get(&self, key: &[K]) -> Option<&T> {
         match key.first() {
             Some(c) => self.childs.get(c).and_then(|node| node.get(&key[1..])),
             None => self.value.as_ref(),
+        }
+    }
+
+    pub fn get_mut(&mut self, key: &[K]) -> Option<&mut T> {
+        match key.first() {
+            Some(c) => self
+                .childs
+                .get_mut(c)
+                .and_then(|node| node.get_mut(&key[1..])),
+            None => self.value.as_mut(),
         }
     }
 }
@@ -38,4 +48,9 @@ fn test() {
     assert_eq!(Some(&123), trie.get(&key[0..5]));
     assert_eq!(None, trie.get(&key[0..4]));
     assert_eq!(None, trie.get(&key[1..4]));
+
+    if let Some(v) = trie.get_mut(&key[..]) {
+        *v = 150;
+    }
+    assert_eq!(Some(&150), trie.get(&key[0..5]));
 }
