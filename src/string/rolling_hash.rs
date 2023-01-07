eaccf1eb21fed009eed1ad6a2664c723fcf11621
katand <katandps@gmail.com>
2023-01-08 08:19:30 +0900

    add test to rolling_hash

--- src/string/rolling_hash.rs
+++ src/string/rolling_hash.rs
@@ -103,3 +103,19 @@ mod rolling_hash_impl {
         }
     }
 }
+
+#[test]
+fn test() {
+    let text = (0..3000000)
+        .map(|i| (i % 256) as u8 as char)
+        .collect::<Vec<_>>();
+    let rh = RollingHash::from(&text[..]);
+    for i in 0..100000 {
+        for j in i + 1.. {
+            if rh.hash(i, i + 10000) == rh.hash(j, j + 10000) {
+                assert_eq!((j - i) % 256, 0);
+                break;
+            }
+        }
+    }
+}
