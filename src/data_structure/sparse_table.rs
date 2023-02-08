2bae9a157aa0b07a56b314e2e11939a46d384a88
katand <katandps@gmail.com>
2023-02-08 13:05:00 +0900

    refactoring

--- src/data_structure/sparse_table.rs
+++ src/data_structure/sparse_table.rs
@@ -16,31 +16,25 @@ mod sparse_table_impl {
 
     #[derive(Clone)]
     pub struct SparseTable<B: Band> {
-        size: usize,
+        pub len: usize,
         table: Vec<Vec<B::M>>,
-    }
-
-    impl<B: Band> SparseTable<B> {
-        pub fn len(&self) -> usize {
-            self.size
-        }
     }
 
     impl<B: Band> From<&[B::M]> for SparseTable<B> {
         fn from(v: &[B::M]) -> Self {
-            let size = v.len();
+            let len = v.len();
             let l = v.len();
             let lg = 63 - l.leading_zeros();
             let mut table = vec![Vec::new(); lg as usize + 1];
             table[0] = v.to_vec();
             let mut k = 1;
-            while 1 << k <= size {
-                table[k] = (0..=size - (1 << k))
+            while 1 << k <= len {
+                table[k] = (0..=len - (1 << k))
                     .map(|i| B::op(&table[k - 1][i], &table[k - 1][i + (1 << (k - 1))]))
                     .collect();
                 k += 1;
             }
-            Self { size, table }
+            Self { len, table }
         }
     }
 
@@ -67,7 +61,7 @@ mod sparse_table_impl {
             writeln!(
                 f,
                 "\n{}",
-                (0..self.len()).map(|i| self.product(i..=i)).join(" ")
+                (0..self.len).map(|i| self.product(i..=i)).join(" ")
             )
         }
     }
