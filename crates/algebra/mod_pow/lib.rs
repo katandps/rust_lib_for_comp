930fdbcf32e0547c8376f12ebd6164bbbc50ae37
katand <katandps@gmail.com>
2023-02-09 15:37:39 +0900

    mod-powをcratesに移動

--- crates/algebra/mod_pow/lib.rs
+++ crates/algebra/mod_pow/lib.rs
@@ -2,13 +2,16 @@
 //! 繰り返し二乗法を使用して値を求める
 //! ## verified by
 //! [ATC002B](https://atcoder.jp/contests/atc002/submissions/26825488)
-use crate::element::integral::Integral;
+use algebra::*;
+use prelude::*;
 
+#[snippet(name = "mod-pow", doc_hidden)]
 pub trait ModPow {
     fn mod_pow(self, exponent: Self, divisor: Self) -> Self;
     fn mod_mul(self, multiplier: Self, divisor: Self) -> Self;
 }
 
+#[snippet(name = "mod-pow", doc_hidden)]
 impl<T: Integral> ModPow for T {
     fn mod_pow(mut self, mut e: T, m: T) -> T {
         self %= m;
@@ -39,14 +42,4 @@ impl<T: Integral> ModPow for T {
             res
         }
     }
-}
-
-#[test]
-fn test() {
-    assert_eq!(100, 5.mod_mul(20, 200));
-    assert_eq!(1, 5.mod_mul(20, 99));
-
-    assert_eq!(32, 2.mod_pow(5, 100000));
-    assert_eq!(2, 2.mod_pow(5, 30));
-    assert_eq!(130944741i64, 12738078907407.mod_pow(9982443567, 998244353))
 }
