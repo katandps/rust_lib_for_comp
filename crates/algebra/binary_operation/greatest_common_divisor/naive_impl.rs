bd1cc6c636c0727ae730c7a14a24729310776d74
katand <katandps@gmail.com>
2023-02-09 12:08:36 +0900

    GCDをcratesに移動

---
+++ crates/algebra/binary_operation/greatest_common_divisor/naive_impl.rs
@@ -0,0 +1,35 @@
+//! # GCD
+//! ナイーブなユークリッドの互除法によるGCDの計算
+use algebra::*;
+use prelude::*;
+
+#[derive(Clone, Debug, Default)]
+pub struct Gcd<S>(PhantomData<fn() -> S>);
+mod gcd_impl {
+    use super::{
+        swap, Associative, Commutative, Debug, Gcd, Idempotent, Magma, RemAssign, Unital, Zero,
+    };
+    impl<S: Clone + Debug + RemAssign + PartialOrd + Zero> Magma for Gcd<S> {
+        type M = S;
+        #[inline]
+        fn op(x: &S, y: &S) -> S {
+            let (mut x, mut y) = (x.clone(), y.clone());
+            if y > x {
+                swap(&mut x, &mut y);
+            }
+            while y != S::zero() {
+                x %= y.clone();
+                swap(&mut x, &mut y);
+            }
+            x
+        }
+    }
+    impl<S: Clone + Debug + RemAssign + PartialOrd + Zero> Associative for Gcd<S> {}
+    impl<S: Clone + Debug + RemAssign + PartialOrd + Zero> Unital for Gcd<S> {
+        fn unit() -> S {
+            S::zero()
+        }
+    }
+    impl<S: Clone + Debug + RemAssign + PartialOrd + Zero> Commutative for Gcd<S> {}
+    impl<S: Clone + Debug + RemAssign + PartialOrd + Zero> Idempotent for Gcd<S> {}
+}
