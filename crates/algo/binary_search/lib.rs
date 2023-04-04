822fb4bb4eabd2f20d685c97a5d3988fe8d90309
katand <katandps@gmail.com>
2023-04-04 19:49:41 +0900

    floatの二分探索を追加

--- crates/algo/binary_search/lib.rs
+++ crates/algo/binary_search/lib.rs
@@ -1,27 +1,43 @@
 //! # 二分探索
 //!
-//! 整数での二分探索を行う
-//!
-//! ## 使い方
-//!
-//! ```
-//! # use binary_search::*;
-//! let src = vec![1, 5, 15, 30, 55, 90, 150];
-//! let res = binary_search(src.len() as i64, -1, |i| src[i as usize] > 55);
-//! assert_eq!(src[res as usize], 90);
-//! ```
+//! 二分探索のテンプレート
 
 use prelude::*;
 
+/// # 整数範囲の二分探索
+///
+/// ## 使い方
+/// ```
+/// # use binary_search::*;
+/// let src = vec![1, 5, 15, 30, 55, 90, 150];
+/// let res = binary_search(src.len() as i64, -1, |i| src[i as usize] > 55);
+/// assert_eq!(src[res as usize], 90);
+/// ```
 #[snippet(name = "binary-search", doc_hidden)]
 pub fn binary_search<F: Fn(i64) -> bool>(mut ok: i64, mut ng: i64, f: F) -> i64 {
     while (ok - ng).abs() > 1 {
-        let mid = (ok + ng) / 2;
-        if f(mid) {
-            ok = mid
-        } else {
-            ng = mid
-        }
+        let mut mid = (ok + ng) / 2;
+        let r = f(mid);
+        swap(&mut mid, if r { &mut ok } else { &mut ng });
+    }
+    ok
+}
+
+/// # 浮動小数点数の二分探索
+///
+/// ## 使い方
+/// ```
+/// # use binary_search::*;
+/// let res = binary_search_float(1e9, 0.0, |m| m >= 5.0);
+/// assert!((5.0-res).abs() < 1e-12);
+/// ```
+#[snippet(name = "binary-search-float", doc_hidden)]
+pub fn binary_search_float<F: Fn(f64) -> bool>(mut ok: f64, mut ng: f64, f: F) -> f64 {
+    const EPS: f64 = 1e-15;
+    while (ok - ng).abs() > EPS {
+        let mut mid = (ok + ng) / 2.0;
+        let r = f(mid);
+        swap(&mut mid, if r { &mut ok } else { &mut ng });
     }
     ok
 }
