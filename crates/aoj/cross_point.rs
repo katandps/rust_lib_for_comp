b427a3f60c4e3b48dbb5b6cf1f13d8d8e1e509cd
katandps <katandps@gmail.com>
2023-09-22 21:44:36 +0900

    add local test

--- crates/aoj/cross_point.rs
+++ crates/aoj/cross_point.rs
@@ -1,21 +1,15 @@
 // verification-helper: PROBLEM https://onlinejudge.u-aizu.ac.jp/courses/library/4/CGL/2/CGL_2_C
 // verification-helper: ERROR 0.0000000001
 use io_util::*;
-use plane_float::{Segment, Vector};
+use plane_float::cgl_2_c;
 
 fn main() {
     let mut io = IO::default();
     for _ in 0..io.v::<usize>() {
         let (x1, y1, x2, y2) = io.v4::<f64, f64, f64, f64>();
         let (x3, y3, x4, y4) = io.v4::<f64, f64, f64, f64>();
-        if let Some(result) = Segment::cross_point(
-            Segment::new(Vector::new(x1, y1), Vector::new(x2, y2)),
-            Segment::new(Vector::new(x3, y3), Vector::new(x4, y4)),
-        ) {
-            io.out(format!("{} {}\n", result.x, result.y))
-        } else {
-            unreachable!()
-        }
+        let (x, y) = cgl_2_c((x1, y1), (x2, y2), (x3, y3), (x4, y4));
+        io.out(format!("{} {}\n", x, y));
     }
     io.flush();
 }
--- crates/geometry/plane_float/lib.rs
+++ crates/geometry/plane_float/lib.rs
@@ -554,3 +554,31 @@ fn test_cgl_1_c() {
         ]
     )
 }
+
+/// <https://onlinejudge.u-aizu.ac.jp/courses/library/4/CGL/2/CGL_2_C>
+pub fn cgl_2_c(p0: (f64, f64), p1: (f64, f64), p2: (f64, f64), p3: (f64, f64)) -> (FValue, FValue) {
+    if let Some(result) = Segment::cross_point(
+        Segment::new(Vector::new(p0.0, p0.1), Vector::new(p1.0, p1.1)),
+        Segment::new(Vector::new(p2.0, p2.1), Vector::new(p3.0, p3.1)),
+    ) {
+        (result.x, result.y)
+    } else {
+        panic!("line is parallel")
+    }
+}
+
+#[test]
+fn test_cgl_2_c() {
+    assert_eq!(
+        cgl_2_c((0.0, 0.0), (2.0, 0.0), (1.0, 1.0), (1.0, -1.0)),
+        (1.0.into(), 0.0.into())
+    );
+    assert_eq!(
+        cgl_2_c((0.0, 0.0), (1.0, 1.0), (0.0, 1.0), (1.0, 0.0)),
+        (0.5.into(), 0.5.into())
+    );
+    assert_eq!(
+        cgl_2_c((0.0, 0.0), (1.0, 1.0), (1.0, 0.0), (0.0, 1.0)),
+        (0.5.into(), 0.5.into())
+    );
+}
