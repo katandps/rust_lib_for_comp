241f1f0304f7d4be020772f72d514af4a553767d
katandps <katandps@gmail.com>
2023-09-23 20:58:14 +0900

    fix verification

--- verifications/aoj/aoj_0275.rs
+++ verifications/aoj/aoj_0275.rs
@@ -1,12 +1,18 @@
 // verification-helper: PROBLEM https://onlinejudge.u-aizu.ac.jp/problems/0275
 
+use aoj_solver::aoj_0275::solve;
 use io_util::*;
+use string_util::*;
 
 fn main() {
     let mut io = IO::default();
-    let xy = io.vec2::<f64, f64>(3);
-    let ans = circle::cgl_7_c(&xy);
-    io.out(format!("{} {} {}\n", ans.0, ans.1, ans.2));
-
-    io.flush();
+    let (s, r) = io.v2::<usize, usize>();
+    let uvw = io.vec3::<usize, usize, i64>(r);
+    let (a, b, q) = io.v3::<usize, usize, usize>();
+    let cd = io.vec2::<usize, usize>(q);
+    let bools = solve(s, &uvw, a, b, &cd);
+    for b in bools {
+        io.out(if b { "Yes" } else { "No" }.line());
+    }
+    io.flush()
 }
