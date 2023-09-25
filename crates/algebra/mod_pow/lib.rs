c18cacbd415c4e648018ccb93498797d4286559c
katandps <katandps@gmail.com>
2023-09-25 13:16:41 +0900

    fix mod_pow for too large mod

--- crates/algebra/mod_pow/lib.rs
+++ crates/algebra/mod_pow/lib.rs
@@ -28,18 +28,39 @@ impl<T: Integral> ModPow for T {
     fn mod_mul(mut self, mut other: T, m: T) -> T {
         self %= m;
         other %= m;
-        if m < T::max_value() >> T::one() {
+        if self <= T::max_value() / other {
             (self * other) % m
         } else {
+            if other > self {
+                swap(&mut self, &mut other);
+            }
             let mut res = T::zero();
             while other > T::zero() {
                 if other & T::one() == T::one() {
-                    res = (res + other) % m;
+                    if T::max_value() - res < self {
+                        // overflow
+                        res = (T::max_value() % m + (self - (T::max_value() - res))) % m;
+                    } else {
+                        res = (res + self) % m;
+                    }
                 }
                 other >>= T::one();
-                self = (self << T::one()) % m;
+                if self << T::one() < self {
+                    // overflow
+                    self = ((self << T::one()) % m + T::max_value() % m + T::one()) % m;
+                } else {
+                    self = (self << T::one()) % m;
+                }
             }
             res
         }
     }
+}
+
+#[test]
+fn test() {
+    assert_eq!(16.mod_mul(16, 200), 56);
+    assert_eq!(16u8.mod_mul(16, 200), 56);
+    assert_eq!(16.mod_pow(2, 200), 56);
+    assert_eq!(16u8.mod_pow(2, 200), 56);
 }
