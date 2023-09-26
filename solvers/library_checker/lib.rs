27ea2efb9b3d0a87d8fc99e4953dd80d9db110ba
katandps <katandps@gmail.com>
2023-09-27 00:15:48 +0900

    add verify floor_sum

--- solvers/library_checker/lib.rs
+++ solvers/library_checker/lib.rs
@@ -2,5 +2,6 @@ pub mod range_kth_smallest;
 pub mod shortest_path;
 pub mod static_range_frequency;
 pub mod strongly_connected_components;
+pub mod sum_of_floor_of_linear;
 pub mod vertex_add_path_sum;
 pub mod vertex_add_path_sum2;
---
+++ verifications/library_checker/sum_of_floor_of_linear.rs
@@ -0,0 +1,5 @@
+//# verification-helper: PROBLEM https://judge.yosupo.jp/problem/sum_of_floor_of_linear
+
+fn main() {
+    library_checker_solver::sum_of_floor_of_linear::solve(io_util::IO::default());
+}
