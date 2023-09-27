ac97c510909caf81c4e8e63f50af0cea4cfac54a
katandps <katandps@gmail.com>
2023-09-27 17:06:54 +0900

    fix test cgl_2_d

--- solvers/aoj/cgl_2_d.rs
+++ solvers/aoj/cgl_2_d.rs
@@ -1,26 +1,32 @@
 //! <https://onlinejudge.u-aizu.ac.jp/courses/library/4/CGL/2/CGL_2_D>
-use float_value::FValue;
+use io_util::*;
 use plane_float::*;
+use string_util::*;
 
-pub fn solve(p0: (f64, f64), p1: (f64, f64), p2: (f64, f64), p3: (f64, f64)) -> FValue {
-    Segment::distance(
-        Segment::new(p0.into(), p1.into()),
-        Segment::new(p2.into(), p3.into()),
-    )
+pub fn solve<IO: ReaderTrait + WriterTrait>(mut io: IO) {
+    for _ in 0..io.v::<usize>() {
+        let p0 = io.v2::<f64, f64>();
+        let p1 = io.v2::<f64, f64>();
+        let p2 = io.v2::<f64, f64>();
+        let p3 = io.v2::<f64, f64>();
+        let ans = Segment::distance(
+            Segment::new(p0.into(), p1.into()),
+            Segment::new(p2.into(), p3.into()),
+        );
+        io.out(ans.line())
+    }
+    io.flush();
 }
 
 #[test]
 fn test() {
-    assert_eq!(
-        solve((0.0, 0.0), (1.0, 0.0), (0.0, 1.0), (1.0, 1.0)),
-        1.0.into()
-    );
-    assert_eq!(
-        solve((0.0, 0.0), (1.0, 0.0), (2.0, 1.0), (1.0, 2.0)),
-        1.4142135624.into()
-    );
-    assert_eq!(
-        solve((-1.0, 0.0), (1.0, 0.0), (0.0, 1.0), (0.0, -1.0)),
-        0.0.into()
-    );
+    solve(io_debug::IODebug::fvalue_assert(
+        "3
+        0 0 1 0 0 1 1 1
+        0 0 1 0 2 1 1 2
+        -1 0 1 0 0 1 0 -1",
+        "1.0000000000
+        1.4142135624
+        0.0000000000",
+    ))
 }
--- verifications/aoj/distance.rs
+++ verifications/aoj/distance.rs
@@ -1,15 +1,6 @@
 // verification-helper: PROBLEM https://onlinejudge.u-aizu.ac.jp/courses/library/4/CGL/2/CGL_2_D
 // verification-helper: ERROR 0.0000000001
-use aoj_solver::cgl_2_d::solve;
-use io_util::*;
-use string_util::*;
 
 fn main() {
-    let mut io = IO::default();
-    for _ in 0..io.v::<usize>() {
-        let (x1, y1, x2, y2) = io.v4::<f64, f64, f64, f64>();
-        let (x3, y3, x4, y4) = io.v4::<f64, f64, f64, f64>();
-        io.out(solve((x1, y1), (x2, y2), (x3, y3), (x4, y4)).line());
-    }
-    io.flush();
+    aoj_solver::cgl_2_d::solve(io_util::IO::default());
 }
