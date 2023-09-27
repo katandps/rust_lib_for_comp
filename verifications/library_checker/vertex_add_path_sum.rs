770c96801616d4ecde6dba0e8b797ac8e6664670
katandps <katandps@gmail.com>
2023-09-27 16:44:55 +0900

    fix tests

--- verifications/library_checker/vertex_add_path_sum.rs
+++ verifications/library_checker/vertex_add_path_sum.rs
@@ -1,6 +1,5 @@
 //# verification-helper: PROBLEM https://judge.yosupo.jp/problem/vertex_add_path_sum
 
 fn main() {
-    let io = io_util::IO::default();
-    library_checker_solver::vertex_add_path_sum::solve(io)
+    library_checker_solver::vertex_add_path_sum::solve(io_util::IO::default())
 }
