fb2cba7e0b84a6d7128fd0fa87c9934fc5845812
katandps <katandps@gmail.com>
2023-09-27 15:05:54 +0900

    optimize tests

--- solvers/aoj/lib.rs
+++ solvers/aoj/lib.rs
@@ -12,6 +12,7 @@ pub mod cgl_3_c;
 pub mod cgl_4_a;
 pub mod cgl_4_c;
 pub mod cgl_5_a;
+pub mod cgl_7_b;
 pub mod cgl_7_c;
 pub mod cgl_7_f;
 pub mod dsl_5_b;
--- verifications/aoj/incircle_of_a_triangle.rs
+++ verifications/aoj/incircle_of_a_triangle.rs
@@ -1,19 +1,6 @@
 // verification-helper: PROBLEM https://onlinejudge.u-aizu.ac.jp/courses/library/4/CGL/7/CGL_7_B
 // verification-helper: ERROR 0.000000001
 
-use circle::Triangle;
-use io_util::*;
-use plane_float::Vector;
-
 fn main() {
-    let mut io = IO::default();
-    let xy = io.vec2::<f64, f64>(3);
-    let p: Vec<_> = xy.into_iter().map(Vector::from).collect();
-    let tri = Triangle::new(p[0], p[1], p[2]);
-    let circle = tri.inner_circle().unwrap();
-    io.out(format!(
-        "{} {} {}\n",
-        circle.center.x, circle.center.y, circle.radius
-    ));
-    io.flush();
+    aoj_solver::cgl_7_b::solve(io_util::IO::default());
 }
