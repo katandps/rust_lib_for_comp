b861b2e8baf06e56768ec1d958d1c85917751eec
katandps <katandps@gmail.com>
2023-09-27 16:43:23 +0900

    fix grl_6_a

--- solvers/aoj/grl_6_a.rs
+++ solvers/aoj/grl_6_a.rs
@@ -1,18 +1,30 @@
 //! <https://onlinejudge.u-aizu.ac.jp/problems/GRL_6_A>
 
 use dinic::Dinic;
+use io_util::*;
+use string_util::*;
 
-pub fn solve(v: usize, _e: usize, uvc: &[(usize, usize, i64)]) -> i64 {
+pub fn solve<IO: ReaderTrait + WriterTrait>(mut io: IO) {
+    let (v, e) = io.v2::<usize, usize>();
+    let uvc = io.vec3::<usize, usize, i64>(e);
     let mut dinic = Dinic::new(v);
-    for &(u, v, c) in uvc {
+    for (u, v, c) in uvc {
         dinic.add_edge(u, v, c);
     }
-    dinic.max_flow(0, v - 1)
+    let ans = dinic.max_flow(0, v - 1);
+    io.out(ans.line());
+    io.flush()
 }
 
 #[test]
 fn test() {
-    let (u, v) = (4, 5);
-    let uvc = vec![(0, 1, 2), (0, 2, 1), (1, 2, 1), (1, 3, 1), (2, 3, 2)];
-    assert_eq!(3, solve(u, v, &uvc));
+    solve(io_debug::IODebug::static_assert(
+        "4 5
+        0 1 2
+        0 2 1
+        1 2 1
+        1 3 1
+        2 3 2",
+        "3",
+    ));
 }
--- verifications/aoj/maximum_flow.rs
+++ verifications/aoj/maximum_flow.rs
@@ -1,13 +1,5 @@
 // verification-helper: PROBLEM https://onlinejudge.u-aizu.ac.jp/problems/GRL_6_A
-use aoj_solver::grl_6_a::solve;
-use io_util::*;
-use string_util::*;
 
 fn main() {
-    let mut io = IO::default();
-    let (v, e) = io.v2::<usize, usize>();
-    let uvc = io.vec3::<usize, usize, i64>(e);
-    let ans = solve(v, e, &uvc);
-    io.out(ans.line());
-    io.flush()
+    aoj_solver::grl_6_a::solve(io_util::IO::default());
 }
