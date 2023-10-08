453831db57d16096455ac1555c3848e2cf2b10cd
katandps <katandps@gmail.com>
2023-10-08 15:58:02 +0900

    fix verification problem

--- verifications/library_checker/vertex_add_subtree_sum/main.rs
+++ verifications/library_checker/vertex_add_subtree_sum/main.rs
@@ -1,4 +1,4 @@
-// verification-helper: PROBLEM https://judge.yosupo.jp/problem/vertex_add_path_sum
+// verification-helper: PROBLEM https://judge.yosupo.jp/problem/vertex_add_subtree_sum
 
 #![cfg_attr(coverage_nightly, feature(coverage_attribute))]
 #[cfg_attr(coverage_nightly, coverage(off))]
