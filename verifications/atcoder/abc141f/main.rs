aa04bc9709a5c57fafda3b4efe014549c9787345
katandps <katandps@gmail.com>
2023-10-16 01:53:44 +0900

    verify abc141f

--- verifications/atcoder/abc141f/main.rs
+++ verifications/atcoder/abc141f/main.rs
@@ -1,6 +1,9 @@
-//! <https://atcoder.jp/contests/abc141/tasks/abc141_f>
-//! # notice
-//! atcoderでのverifyは2023/09現在不可?
+// verification-helper: PROBLEM https://atcoder.jp/contests/abc141/tasks/abc141_f
+#![cfg_attr(coverage_nightly, feature(coverage_attribute))]
+#[cfg_attr(coverage_nightly, coverage(off))]
+fn main() {
+    solve(io_util::IO::default());
+}
 use bit_matrix::*;
 use io_util::*;
 use string_util::*;
@@ -33,28 +36,20 @@ pub fn solve<IO: ReaderTrait + WriterTrait>(mut io: IO) {
 }
 
 #[test]
-fn test1() {
+fn test() {
     solve(io_debug::IODebug::static_assert(
         "3
     3 6 5",
         "12",
-    ))
-}
-
-#[test]
-fn test2() {
+    ));
     solve(io_debug::IODebug::static_assert(
         "4
         23 36 66 65",
         "188",
-    ))
-}
-
-#[test]
-fn test3() {
+    ));
     solve(io_debug::IODebug::static_assert(
         "20
         1008288677408720767 539403903321871999 1044301017184589821 215886900497862655 504277496111605629 972104334925272829 792625803473366909 972333547668684797 467386965442856573 755861732751878143 1151846447448561405 467257771752201853 683930041385277311 432010719984459389 319104378117934975 611451291444233983 647509226592964607 251832107792119421 827811265410084479 864032478037725181",
         "2012721721873704572",
-    ))
+    ));
 }
