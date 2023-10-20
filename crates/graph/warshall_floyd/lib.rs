b7bd7d6313429268f031dfbb7bedace6170c1097
katandps <katandps@gmail.com>
2023-10-20 17:46:32 +0900

    verify warshall_floyd

--- crates/graph/warshall_floyd/lib.rs
+++ crates/graph/warshall_floyd/lib.rs
@@ -1,64 +1,60 @@
 //! # ワーシャルフロイド法
+//!
+//!  ## 計算量
+//! 頂点数をNとして $O(N^3)$
+//!
 use algebra::{BoundedAbove, Zero};
 use graph::GraphTrait;
+use min_max_macro::{chmin, min};
 use prelude::*;
 
-macro_rules! chmin {($base:expr, $($cmps:expr),+ $(,)*) => {{let cmp_min = min!($($cmps),+);if $base > cmp_min {$base = cmp_min;true} else {false}}};}
-macro_rules! min {
-    ($a:expr $(,)*) => {{$a}};
-    ($a:expr, $b:expr $(,)*) => {{if $a > $b {$b} else {$a}}};
-    ($a:expr, $($rest:expr),+ $(,)*) => {{let b = min!($($rest),+);if $a > b {b} else {$a}}};
-}
-
-///
-/// ワーシャルフロイド法で(i,j)間の最短距離を求める
-/// ## 計算量
-/// 頂点数をNとしてO(N^3)
-/// ```
-/// use adjacency_list::Graph;
-/// use warshall_floyd::WarshallFloyd;
-///
-/// let mut graph = Graph::new(5);
-/// graph.add_arc(0, 1, 1);
-/// graph.add_arc(1, 2, 2);
-/// graph.add_arc(2, 3, 3);
-/// graph.add_arc(3, 4, 4);
-/// graph.add_arc(4, 0, 5);
-/// let wf = graph.warshall_froyd();
-/// assert_eq!(1, wf[0][1]);
-/// assert_eq!(3, wf[0][2]);
-/// assert_eq!(6, wf[0][3]);
-/// assert_eq!(10, wf[0][4]);
-/// assert_eq!(12, wf[3][2]);
-/// ```
 #[snippet(name = "warshall-floyd", doc_hidden)]
-pub trait WarshallFloyd<W> {
-    fn warshall_froyd(&self) -> Vec<Vec<W>>;
-}
-
+pub use warshall_floyd_impl::WarshallFloyd;
 #[snippet(name = "warshall-floyd", doc_hidden)]
-impl<G: GraphTrait<Weight = W>, W: Copy + PartialOrd + BoundedAbove + Add<Output = W> + Zero>
-    WarshallFloyd<W> for G
-{
-    fn warshall_froyd(&self) -> Vec<Vec<W>> {
-        let mut dist = vec![vec![W::max_value(); self.size()]; self.size()];
-        dist.iter_mut()
-            .enumerate()
-            .for_each(|(i, reti)| reti[i] = W::zero());
-        (0..self.size()).for_each(|src| {
-            for (dst, weight) in self.edges(src) {
-                chmin!(dist[src][dst], weight);
-            }
-        });
-        for i in 0..self.size() {
-            for j in 0..self.size() {
-                for k in 0..self.size() {
-                    if dist[j][i] < W::max_value() && dist[i][k] < W::max_value() {
-                        chmin!(dist[j][k], dist[j][i] + dist[i][k]);
+mod warshall_floyd_impl {
+    use super::{chmin, min, Add, BoundedAbove, GraphTrait, Zero};
+    pub struct WarshallFloyd<W> {
+        dist: Vec<Vec<W>>,
+    }
+
+    impl<W: Copy + PartialOrd + BoundedAbove + Add<Output = W> + Zero> WarshallFloyd<W> {
+        pub fn build<G: GraphTrait<Weight = W>>(graph: &G) -> Self {
+            let mut dist = vec![vec![W::max_value(); graph.size()]; graph.size()];
+            dist.iter_mut()
+                .enumerate()
+                .for_each(|(i, reti)| reti[i] = W::zero());
+            (0..graph.size()).for_each(|src| {
+                for (dst, weight) in graph.edges(src) {
+                    chmin!(dist[src][dst], weight);
+                }
+            });
+            for i in 0..graph.size() {
+                for j in 0..graph.size() {
+                    for k in 0..graph.size() {
+                        if dist[j][i] < W::max_value() && dist[i][k] < W::max_value() {
+                            chmin!(dist[j][k], dist[j][i] + dist[i][k]);
+                        }
                     }
                 }
             }
+            Self { dist }
         }
-        dist
+
+        pub fn dist(&self, src: usize, dst: usize) -> W {
+            self.dist[src][dst]
+        }
+
+        /// # 負のサイクル判定
+        ///
+        /// ## 計算量
+        /// $O(N)$
+        pub fn contains_negative_cycle(&self) -> bool {
+            for (i, dist) in self.dist.iter().enumerate() {
+                if dist[i] < W::zero() {
+                    return true;
+                }
+            }
+            return false;
+        }
     }
 }
