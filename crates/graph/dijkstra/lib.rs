7a92f8740168b37b8a80e7fab03c498eb5b61eff
katandps <katandps@gmail.com>
2023-10-26 01:16:04 +0900

    verify tree_diameter

--- crates/graph/dijkstra/lib.rs
+++ crates/graph/dijkstra/lib.rs
@@ -7,22 +7,24 @@
 //! O(N \log N)
 use algebra::{BoundedAbove, Zero};
 use graph::GraphTrait;
+use min_max_macro::{chmax, max};
 use prelude::*;
 
 #[snippet(name = "dijkstra", doc_hidden)]
 pub struct Dijkstra<W> {
     pub dist: Vec<W>,
     prev: Vec<usize>,
+    src: usize,
 }
 
 #[snippet(name = "dijkstra", doc_hidden)]
 impl<W: Copy + BoundedAbove + Add<Output = W> + PartialEq + Ord + Zero> Dijkstra<W> {
-    pub fn calc<G: GraphTrait<Weight = W>>(g: &G, l: usize) -> Self {
+    pub fn calc<G: GraphTrait<Weight = W>>(g: &G, src: usize) -> Self {
         let mut dist = vec![W::max_value(); g.size()];
         let mut prev = vec![g.size(); g.size()];
         let mut heap = BinaryHeap::new();
-        dist[l] = W::zero();
-        heap.push((Reverse(W::zero()), l));
+        dist[src] = W::zero();
+        heap.push((Reverse(W::zero()), src));
         while let Some((Reverse(d), src)) = heap.pop() {
             if dist[src] != d {
                 continue;
@@ -35,7 +37,7 @@ impl<W: Copy + BoundedAbove + Add<Output = W> + PartialEq + Ord + Zero> Dijkstra
                 }
             });
         }
-        Self { dist, prev }
+        Self { dist, prev, src }
     }
 
     pub fn path(&self, mut to: usize) -> Vec<usize> {
@@ -46,5 +48,26 @@ impl<W: Copy + BoundedAbove + Add<Output = W> + PartialEq + Ord + Zero> Dijkstra
         }
         path.reverse();
         path
+    }
+
+    pub fn farthest_vertex(&self) -> usize {
+        let (mut dist, mut v) = (W::zero(), self.src);
+        for (i, d) in self.dist.iter().enumerate() {
+            if *d == W::max_value() {
+                continue;
+            }
+            if chmax!(dist, *d) {
+                v = i;
+            }
+        }
+        v
+    }
+
+    pub fn diameter<G: GraphTrait<Weight = W>>(graph: &G) -> (Self, usize, usize) {
+        let d1 = Self::calc(graph, 0);
+        let l = d1.farthest_vertex();
+        let d2 = Self::calc(graph, l);
+        let r = d2.farthest_vertex();
+        (d2, l, r)
     }
 }
---
+++ verifications/library_checker/tree_diameter/main.rs
@@ -0,0 +1,40 @@
+// verification-helper: PROBLEM https://judge.yosupo.jp/problem/tree_diameter
+#![cfg_attr(coverage_nightly, feature(coverage_attribute))]
+#[cfg_attr(coverage_nightly, coverage(off))]
+fn main() {
+    solve(io_util::IO::default());
+}
+use adjacency_list::Graph;
+use dijkstra::*;
+use io_util::*;
+use string_util::*;
+
+pub fn solve<IO: ReaderTrait + WriterTrait>(mut io: IO) {
+    let n = io.v::<usize>();
+    let abc = io.vec3::<usize, usize, i64>(n - 1);
+    let mut graph = Graph::new(n);
+    for (a, b, c) in abc {
+        graph.add_edge(a, b, c);
+    }
+    let (dist, _l, r) = Dijkstra::diameter(&graph);
+    let path = dist.path(r);
+    io.out(format!("{} {}", dist.dist[r], path.len()).line());
+    io.out(path.join(" ").line());
+    io.flush()
+}
+
+#[test]
+fn test() {
+    solve(io_debug::IODebug::static_assert(
+        "8
+        0 1 5
+        1 2 3
+        2 3 1
+        1 4 2
+        4 7 4
+        1 5 7
+        2 6 5",
+        "15 4
+        6 2 1 5",
+    ))
+}
