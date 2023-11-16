c5fa9bc231ce540171b802206c9f4274b81b2975
katandps <katandps@gmail.com>
2023-11-17 01:12:34 +0900

    fix min_max snippet

--- crates/util/min_max_macro/lib.rs
+++ crates/util/min_max_macro/lib.rs
@@ -4,71 +4,72 @@ use prelude::snippet;
 
 #[snippet(name = "min_max", doc_hidden)]
 #[rustfmt::skip]
-mod min_max_macro_impl {
-    #[macro_export]
-    macro_rules! min {
-        ($a:expr $(,)*) => {{$a}};
-        ($a:expr, $b:expr $(,)*) => {
-            {
-                let (ar, br) = ($a, $b);
-                if ar > br {br} else {ar}
-            }
-        };
-        ($a:expr, $($rest:expr),+ $(,)*) => {
-            {
-                let b = min!($($rest),+);
-                let ar = $a;
-                if ar > b {b} else {ar}
-            }
-        };
-    }
-
-    #[macro_export]
-    macro_rules! chmin {
-        ($base:expr, $($cmps:expr),+ $(,)*) => {
-            {
-                let cmp_min = min!($($cmps),+);
-                if $base > cmp_min {
-                    $base = cmp_min;
-                    true
-                } else {
-                    false
-                }
-            }
-        };
-    }
-
-    #[macro_export]
-    macro_rules! max {
-        ($a:expr $(,)*) => {{$a}};
-        ($a:expr, $b:expr $(,)*) => {
-            {
-                let (ar, br) = ($a, $b);
-                if ar > br {ar} else {br}
-            }
-        };
-        ($a:expr, $($rest:expr),+ $(,)*) => {
-            {
-                let b = max!($($rest),+);
-                let ar = $a;
-                if ar > b {ar} else {b}
-            }
-        };
-    }
-
-    #[macro_export]
-    macro_rules! chmax {
-        ($base:expr, $($cmps:expr),+ $(,)*) => {
-            {
-                let cmp_max = max!($($cmps),+);
-                if $base < cmp_max {
-                    $base = cmp_max;
-                    true
-                } else {
-                    false
-                }}
-        };
+#[macro_export]
+macro_rules! min {
+    ($a:expr $(,)*) => {{$a}};
+    ($a:expr, $b:expr $(,)*) => {
+        {
+            let (ar, br) = ($a, $b);
+            if ar > br {br} else {ar}
+        }
+    };
+    ($a:expr, $($rest:expr),+ $(,)*) => {
+        {
+            let b = min!($($rest),+);
+            let ar = $a;
+            if ar > b {b} else {ar}
+        }
+    };
 }
+#[snippet(name = "min_max", doc_hidden)]
+#[rustfmt::skip]
+#[macro_export]
+macro_rules! chmin {
+    ($base:expr, $($cmps:expr),+ $(,)*) => {
+        {
+            let cmp_min = min!($($cmps),+);
+            if $base > cmp_min {
+                $base = cmp_min;
+                true
+            } else {
+                false
+            }
+        }
+    };
+}
+#[snippet(name = "min_max", doc_hidden)]
+#[rustfmt::skip]
+#[macro_export]
+macro_rules! max {
+    ($a:expr $(,)*) => {{$a}};
+    ($a:expr, $b:expr $(,)*) => {
+        {
+            let (ar, br) = ($a, $b);
+            if ar > br {ar} else {br}
+        }
+    };
+    ($a:expr, $($rest:expr),+ $(,)*) => {
+        {
+            let b = max!($($rest),+);
+            let ar = $a;
+            if ar > b {ar} else {b}
+        }
+    };
+}
+#[snippet(name = "min_max", doc_hidden)]
+#[rustfmt::skip]
+#[macro_export]
+macro_rules! chmax {
+    ($base:expr, $($cmps:expr),+ $(,)*) => {
+        {
+            let cmp_max = max!($($cmps),+);
+            if $base < cmp_max {
+                $base = cmp_max;
+                true
+            } else {
+                false
+            }}
+    };
 }
 
 #[cfg(test)]
