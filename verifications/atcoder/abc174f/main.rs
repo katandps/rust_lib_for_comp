9e984edc33ce033459b1c3a1fd3824f185c71525
katandps <katandps@gmail.com>
2023-11-17 00:57:45 +0900

    verify abc174_f

--- verifications/atcoder/abc174f/main.rs
+++ verifications/atcoder/abc174f/main.rs
@@ -1,3 +1,4 @@
+// verification-helper: PROBLEM https://atcoder.jp/contests/abc174/tasks/abc174_f
 //! # Static Range Set Query(区間の種類数)
 //! クエリを$r$でソートして小さいほうから見る。
 //!
@@ -9,19 +10,27 @@
 //! - $A$: 数列 $0 <= a_i < KINDS$
 //! - $LR$: クエリ $[L, R)$
 //!
-//! ## verify
-//! [ABC174F](https://atcoder.jp/contests/abc174/submissions/31141642)
+#![cfg_attr(coverage_nightly, feature(coverage_attribute))]
+#[cfg_attr(coverage_nightly, coverage(off))]
+fn main() {
+    solve(io_util::IO::default());
+}
 
 use addition::Addition;
 use binary_indexed_tree::BinaryIndexedTree;
+use io_util::*;
 use prelude::*;
 use range_traits::*;
+use string_util::*;
 
 const KINDS: usize = 500_000;
 
-pub fn solve(n: usize, q: usize, a: Vec<usize>, lr: Vec<(usize, usize)>) -> Vec<i64> {
+pub fn solve<IO: ReaderTrait + WriterTrait>(mut io: IO) {
+    let (n, q) = io.v2::<usize, usize>();
+    let a = io.vec::<usize>(n);
+    let lr = io.vec2::<usize, usize>(q);
     let mut queries = BinaryHeap::new(); // Reverse((r, l, i))
-    (0..q).for_each(|i| queries.push(Reverse((lr[i].1, lr[i].0, i))));
+    (0..q).for_each(|i| queries.push(Reverse((lr[i].1, lr[i].0 - 1, i))));
     let mut ans = vec![0; q];
     let mut last_pos = vec![None; KINDS + 1]; // 最後に見つけた位置
     let mut bit = BinaryIndexedTree::<Addition<i64>>::from(n);
@@ -40,34 +49,44 @@ pub fn solve(n: usize, q: usize, a: Vec<usize>, lr: Vec<(usize, usize)>) -> Vec<
             ans[query.2] = bit.product(query.1..query.0);
         }
     }
-    ans
+    io.out(ans.join("\n").line());
+    io.flush();
 }
 
 #[test]
 fn test() {
-    assert_eq!(
-        vec![2, 3, 1],
-        solve(4, 3, vec![1, 2, 1, 3], vec![(0, 3), (1, 4), (2, 3)])
-    );
-
-    assert_eq!(
-        vec![1, 2, 2, 1, 2, 2, 6, 3, 3, 3],
-        solve(
-            10,
-            10,
-            vec![2, 5, 6, 5, 2, 1, 7, 9, 7, 2],
-            vec![
-                (4, 5),
-                (1, 4),
-                (5, 7),
-                (1, 2),
-                (6, 8),
-                (6, 9),
-                (0, 8),
-                (5, 9),
-                (7, 10),
-                (5, 8)
-            ]
-        )
-    )
+    solve(io_debug::IODebug::static_assert(
+        "4 3
+    1 2 1 3
+    1 3
+    2 4
+    3 3",
+        "2
+        3
+        1",
+    ));
+    solve(io_debug::IODebug::static_assert(
+        "10 10
+        2 5 6 5 2 1 7 9 7 2
+        5 5
+        2 4
+        6 7
+        2 2
+        7 8
+        7 9
+        1 8
+        6 9
+        8 10
+        6 8",
+        "1
+        2
+        2
+        1
+        2
+        2
+        6
+        3
+        3
+        3",
+    ));
 }
