b896170e88c23c07923026f05a1e811fa3d5f84b
katandps <katandps@gmail.com>
2024-02-14 13:38:07 +0900

    fix workflow

--- crates/rust_lib_for_comp/geometry/plane_int.rs
+++ crates/rust_lib_for_comp/geometry/plane_int.rs
@@ -43,24 +43,6 @@ mod plane_float_impl {
         ///
         /// ## 使い方
         /// (0, 0)をうまく比較できないので先に除いておくこと
-        ///
-        /// ```
-        /// # use itertools::Itertools;
-        /// # use plane_int::*;
-        /// let pts = [
-        ///     (1, 0),
-        ///     (1, 1),
-        ///     (0, 1),
-        ///     (-1, 1),
-        ///     (-1, 0),
-        ///     (-1, -1),
-        ///     (0, -1),
-        ///     (1, -1),
-        /// ];
-        /// for (i, j) in (0..8).cartesian_product(0..8) {
-        ///     assert_eq!(Point::arg_cmp(&pts[i].into(), &pts[j].into()), i.cmp(&j));
-        /// }
-        /// ```
         pub fn arg_cmp(p1: &Self, p2: &Self) -> Ordering {
             ((p1.y, p1.x) < (0, 0))
                 .cmp(&((p2.y, p2.x) < (0, 0)))
@@ -71,5 +53,23 @@ mod plane_float_impl {
         fn from(p: (i64, i64)) -> Point {
             Point { x: p.0, y: p.1 }
         }
+    }
+}
+
+#[test]
+fn arg_cmp() {
+    use itertools::Itertools;
+    let pts = [
+        (1, 0),
+        (1, 1),
+        (0, 1),
+        (-1, 1),
+        (-1, 0),
+        (-1, -1),
+        (0, -1),
+        (1, -1),
+    ];
+    for (i, j) in (0..8).cartesian_product(0..8) {
+        assert_eq!(Point::arg_cmp(&pts[i].into(), &pts[j].into()), i.cmp(&j));
     }
 }
--- crates/verifications/library_checker/string/z_algorithm.rs
+++ crates/verifications/library_checker/string/z_algorithm.rs
@@ -1,28 +1,23 @@
-// // verification-helper: PROBLEM https://judge.yosupo.jp/problem/zalgorithm
-// #![cfg_attr(coverage_nightly, feature(coverage_attribute))]
-// #[cfg_attr(coverage_nightly, coverage(off))]
-// fn main() {
-//     solve(io_util::IO::default())
-// }
-// use io_util::*;
-// use string_util::*;
-// use z_algorithm::z;
+use rust_lib_for_comp::string::z_algorithm::z;
+use rust_lib_for_comp::util::io_util::{ReadHelper, ReaderTrait};
+use rust_lib_for_comp::util::string_util::JoinTrait;
+use verify::{LibraryChecker, Solver};
 
-// pub fn solve<IO: ReaderTrait + WriterTrait>(mut io: IO) {
-//     let s = io.chars();
-//     io.out(z(&s).join(" "));
-//     io.flush();
-// }
-// #[test]
-// fn test() {
-//     solve(io_debug::IODebug::static_assert("abcbcba", "7 0 0 0 0 0 1"));
-//     solve(io_debug::IODebug::static_assert(
-//         "mississippi",
-//         "11 0 0 0 0 0 0 0 0 0 0",
-//     ));
-//     solve(io_debug::IODebug::static_assert(
-//         "ababacaca",
-//         "9 0 3 0 1 0 1 0 1",
-//     ));
-//     solve(io_debug::IODebug::static_assert("aaaaa", "5 4 3 2 1"));
-// }
+#[derive(LibraryChecker)]
+pub struct ZAlgorithm;
+impl verify::Solver for ZAlgorithm {
+    const PROBLEM_ID: &'static str = "zalgorithm";
+    const TIME_LIMIT_MILLIS: u64 = 5000;
+    fn solve(read: impl std::io::Read, mut write: impl std::io::Write) {
+        let mut reader = ReadHelper::new(read);
+        let s = reader.chars();
+        writeln!(write, "{}", z(&s).join(" ")).unwrap()
+    }
+}
+#[test]
+fn test() {
+    ZAlgorithm::assert("abcbcba", "7 0 0 0 0 0 1");
+    ZAlgorithm::assert("mississippi", "11 0 0 0 0 0 0 0 0 0 0");
+    ZAlgorithm::assert("ababacaca", "9 0 3 0 1 0 1 0 1");
+    ZAlgorithm::assert("aaaaa", "5 4 3 2 1");
+}
