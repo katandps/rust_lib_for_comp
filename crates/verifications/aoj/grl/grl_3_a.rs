c3f100cc89aedb6c907f85a64e0fb7483f2a4af1
katandps <katandps@gmail.com>
2024-03-01 00:12:55 +0900

    verify Articulation Points

---
+++ crates/verifications/aoj/grl/grl_3_a.rs
@@ -0,0 +1,47 @@
+//! # Articulation Points(関節点)
+
+use rust_lib_for_comp::graph::{adjacency_list::Graph, low_link::LowLink};
+use rust_lib_for_comp::util::io_util::*;
+use verify::{AizuOnlineJudge, Solver};
+
+#[derive(AizuOnlineJudge)]
+pub struct Grl3A;
+impl verify::Solver for Grl3A {
+    const PROBLEM_ID: &'static str = "GRL_3_A";
+    const TIME_LIMIT_MILLIS: u64 = 1000;
+    fn solve(read: impl std::io::Read, mut write: impl std::io::Write) {
+        let mut reader = ReadHelper::new(read);
+        let (v, e) = reader.v2::<usize, usize>();
+        let st = reader.vec2::<usize, usize>(e);
+        let mut graph = Graph::new(v);
+        for (s, t) in st {
+            graph.add_edge(s, t, ());
+        }
+        let ll = LowLink::build(&graph);
+        for a in &ll.articulation {
+            writeln!(write, "{}", a).unwrap()
+        }
+    }
+}
+
+#[test]
+fn test() {
+    Grl3A::assert(
+        "4 4
+        0 1
+        0 2
+        1 2
+        2 3",
+        "2",
+    );
+    Grl3A::assert(
+        "5 4
+        0 1
+        1 2
+        2 3
+        3 4",
+        "1
+        2
+        3",
+    );
+}
--- crates/verifications/aoj/grl/mod.rs
+++ crates/verifications/aoj/grl/mod.rs
@@ -3,6 +3,7 @@ pub mod grl_1_a;
 pub mod grl_1_b;
 pub mod grl_1_c;
 pub mod grl_2_a;
+pub mod grl_3_a;
 pub mod grl_3_b;
 pub mod grl_6_a;
 pub mod grl_6_b;
